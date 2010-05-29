8ddf99140d13520a4b531fbb44273a28668872c0
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-05-29 21:29:55 +0000

    The faces context that provides information about variables, request, response writer, ...

---
+++ src/asf-contexts-faces.adb
@@ -0,0 +1,128 @@
+-----------------------------------------------------------------------
+--  asf-contexts.faces -- Faces Contexts
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with EL.Variables;
+with Ada.Task_Attributes;
+package body ASF.Contexts.Faces is
+
+   package Task_Context is new Ada.Task_Attributes
+     (Faces_Context_Access, null);
+
+   --  ------------------------------
+   --  Get the response writer to write the response stream.
+   --  ------------------------------
+   function Get_Response_Writer (Context : Faces_Context)
+                                 return ASF.Contexts.Writer.ResponseWriter_Access is
+   begin
+      return Context.Writer;
+   end Get_Response_Writer;
+
+   --  ------------------------------
+   --  Set the response writer to write to the response stream.
+   --  ------------------------------
+   procedure Set_Response_Writer (Context : in out Faces_Context;
+                                  Writer  : in ASF.Contexts.Writer.ResponseWriter_Access) is
+   begin
+      Context.Writer := Writer;
+   end Set_Response_Writer;
+
+   --  ------------------------------
+   --  Get the EL context for evaluating expressions.
+   --  ------------------------------
+   function Get_ELContext (Context : in Faces_Context)
+                           return EL.Contexts.ELContext_Access is
+   begin
+      return Context.Context;
+   end Get_ELContext;
+
+   --  ------------------------------
+   --  Set the EL context for evaluating expressions.
+   --  ------------------------------
+   procedure Set_ELContext (Context   : in out Faces_Context;
+                            ELContext : in EL.Contexts.ELContext_Access) is
+   begin
+      Context.Context := ELContext;
+   end Set_ELContext;
+
+   --  ------------------------------
+   --  Set the attribute having given name with the value.
+   --  ------------------------------
+   procedure Set_Attribute (Context : in out Faces_Context;
+                            Name    : in String;
+                            Value   : in EL.Objects.Object) is
+      use EL.Variables;
+      Mapper : constant access VariableMapper'Class := Context.Context.Get_Variable_Mapper;
+   begin
+      Mapper.Bind (Name, Value);
+   end Set_Attribute;
+
+   --  ------------------------------
+   --  Set the attribute having given name with the value.
+   --  ------------------------------
+   procedure Set_Attribute (Context : in out Faces_Context;
+                            Name    : in Unbounded_String;
+                            Value   : in EL.Objects.Object) is
+   begin
+      Context.Set_Attribute (To_String (Name), Value);
+   end Set_Attribute;
+
+   --  ------------------------------
+   --  Get a request parameter
+   --  ------------------------------
+   function Get_Parameter (Context : Faces_Context;
+                           Name    : String) return String is
+   begin
+      return AWS.Status.Parameter (Context.Request.all, Name);
+   end Get_Parameter;
+
+   --  ------------------------------
+   --  Get the request
+   --  ------------------------------
+   function Get_Request (Context : Faces_Context)
+                         return access AWS.Status.Data is
+   begin
+      return Context.Request;
+   end Get_Request;
+
+   --  ------------------------------
+   --  Set the request
+   --  ------------------------------
+   procedure Set_Request (Context : in out Faces_Context;
+                          Request : access AWS.Status.Data) is
+   begin
+      Context.Request := Request;
+   end Set_Request;
+
+   --  ------------------------------
+   --  Get the current faces context.  The faces context is saved
+   --  in a per-thread/task attribute.
+   --  ------------------------------
+   function Current return Faces_Context_Access is
+   begin
+      return Task_Context.Value;
+   end Current;
+
+   --  ------------------------------
+   --  Set the current faces context in the per-thread/task attribute.
+   --  ------------------------------
+   procedure Set_Current (Context : Faces_Context_Access) is
+   begin
+      Task_Context.Set_Value (Context);
+   end Set_Current;
+
+end ASF.Contexts.Faces;
---
+++ src/asf-contexts-faces.ads
@@ -0,0 +1,97 @@
+-----------------------------------------------------------------------
+--  asf-contexts.faces -- Faces Contexts
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Contexts.Writer;
+with EL.Objects;
+with EL.Contexts;
+with Ada.Strings.Unbounded;
+with AWS.Status;
+
+--  The <b>Faces_Context</b> is an object passed to the component tree and
+--  bean actions to provide the full context in which the view is rendered
+--  or evaluated.  The faces context gives access to the bean variables,
+--  the request and its parameters, the response writer to write on the
+--  output stream.
+--
+--  The <b>Faces_Context</b> is never shared: it is specific to each request.
+package ASF.Contexts.Faces is
+
+   use Ada.Strings.Unbounded;
+
+   type Faces_Context is tagged private;
+
+   type Faces_Context_Access is access all Faces_Context'Class;
+
+   --  Get the response writer to write the response stream.
+   function Get_Response_Writer (Context : Faces_Context)
+     return ASF.Contexts.Writer.ResponseWriter_Access;
+
+   --  Set the response writer to write to the response stream.
+   procedure Set_Response_Writer (Context : in out Faces_Context;
+                                  Writer  : in ASF.Contexts.Writer.ResponseWriter_Access);
+
+   --  Get the EL context for evaluating expressions.
+   function Get_ELContext (Context : in Faces_Context)
+                           return EL.Contexts.ELContext_Access;
+
+   --  Set the EL context for evaluating expressions.
+   procedure Set_ELContext (Context   : in out Faces_Context;
+                            ELContext : in EL.Contexts.ELContext_Access);
+
+   --  Set the attribute having given name with the value.
+   procedure Set_Attribute (Context : in out Faces_Context;
+                            Name    : in String;
+                            Value   : in EL.Objects.Object);
+
+   --  Set the attribute having given name with the value.
+   procedure Set_Attribute (Context : in out Faces_Context;
+                            Name    : in Unbounded_String;
+                            Value   : in EL.Objects.Object);
+
+   --  Get a request parameter
+   function Get_Parameter (Context : Faces_Context;
+                           Name    : String) return String;
+
+   --  Get the request
+   function Get_Request (Context : Faces_Context) return access AWS.Status.Data;
+
+   --  Set the request
+   procedure Set_Request (Context : in out Faces_Context;
+                          Request : access AWS.Status.Data);
+
+   --  Get the current faces context.  The faces context is saved
+   --  in a per-thread/task attribute.
+   function Current return Faces_Context_Access;
+
+   --  Set the current faces context in the per-thread/task attribute.
+   procedure Set_Current (Context : Faces_Context_Access);
+
+private
+
+   type Faces_Context is tagged record
+      --  The response writer.
+      Writer : ASF.Contexts.Writer.ResponseWriter_Access;
+
+      --  The expression context;
+      Context : EL.Contexts.ELContext_Access;
+
+      --  The request
+      Request : access AWS.Status.Data;
+   end record;
+
+end ASF.Contexts.Faces;

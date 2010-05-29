9f7941e32c9e73d732a9bf815a4caac95cb62011
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-05-29 21:56:18 +0000

    Pre-defined tags from JSTL: <c:if>, <c:set>, <c:choose>

---
+++ src/asf-views-nodes-core.adb
@@ -0,0 +1,256 @@
+-----------------------------------------------------------------------
+--  nodes-core -- Core nodes
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package body ASF.Views.Nodes.Core is
+
+   --  ------------------------------
+   --  Set Tag
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Create the Set Tag
+   --  ------------------------------
+   function Create_Set_Tag_Node (Name       : Unbounded_String;
+                                 Parent     : Tag_Node_Access;
+                                 Attributes : Tag_Attribute_Array_Access)
+                                 return Tag_Node_Access is
+      Node : constant Set_Tag_Node_Access := new Set_Tag_Node;
+   begin
+      Node.Name       := Name;
+      Node.Parent     := Parent;
+      Node.Attributes := Attributes;
+      Node.Value      := Find_Attribute (Attributes, "value");
+      Node.Var        := Find_Attribute (Attributes, "var");
+      return Node.all'Access;
+   end Create_Set_Tag_Node;
+
+   --  ------------------------------
+   --  Build the component tree from the tag node and attach it as
+   --  the last child of the given parent.  Calls recursively the
+   --  method to create children.
+   --  ------------------------------
+   overriding
+   procedure Build_Components (Node    : access Set_Tag_Node;
+                               Parent  : in UIComponent_Access;
+                               Context : in out Facelet_Context'Class) is
+      pragma Unreferenced (Parent);
+
+      Value : constant EL.Objects.Object := Get_Value (Node.Value.all, Context);
+   begin
+      Context.Set_Attribute (Node.Var.Value, Value);
+   end Build_Components;
+
+   --  ------------------------------
+   --  If Tag
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Create the If Tag
+   --  ------------------------------
+   function Create_If_Tag_Node (Name       : Unbounded_String;
+                                Parent     : Tag_Node_Access;
+                                Attributes : Tag_Attribute_Array_Access)
+                                return Tag_Node_Access is
+      Node : constant If_Tag_Node_Access := new If_Tag_Node;
+   begin
+      Node.Name       := Name;
+      Node.Parent     := Parent;
+      Node.Attributes := Attributes;
+      Node.Condition  := Find_Attribute (Attributes, "test");
+      Node.Var        := Find_Attribute (Attributes, "var");
+      return Node.all'Access;
+   end Create_If_Tag_Node;
+
+   --  ------------------------------
+   --  Build the component tree from the tag node and attach it as
+   --  the last child of the given parent.  Calls recursively the
+   --  method to create children.
+   --  ------------------------------
+   overriding
+   procedure Build_Components (Node    : access If_Tag_Node;
+                               Parent  : in UIComponent_Access;
+                               Context : in out Facelet_Context'Class) is
+      Value : constant EL.Objects.Object := Get_Value (Node.Condition.all, Context);
+   begin
+      if Node.Var /= null then
+         Context.Set_Attribute (Node.Var.Value, Value);
+      end if;
+      if EL.Objects.To_Boolean (Value) then
+         Tag_Node (Node.all).Build_Children (Parent, Context);
+      end if;
+   end Build_Components;
+
+   --  ------------------------------
+   --  Choose Tag
+   --  ------------------------------
+
+   --  ------------------------------
+   --  Create the Choose Tag
+   --  ------------------------------
+   function Create_Choose_Tag_Node (Name       : Unbounded_String;
+                                    Parent     : Tag_Node_Access;
+                                    Attributes : Tag_Attribute_Array_Access)
+                                    return Tag_Node_Access is
+      Node : constant Choose_Tag_Node_Access := new Choose_Tag_Node;
+   begin
+      Node.Name       := Name;
+      Node.Parent     := Parent;
+      Node.Attributes := Attributes;
+      return Node.all'Access;
+   end Create_Choose_Tag_Node;
+
+   --  ------------------------------
+   --  Freeze the tag node tree and perform any initialization steps
+   --  necessary to build the components efficiently.
+   --  Prepare the evaluation of choices by identifying the <c:when> and
+   --  <c:otherwise> conditions.
+   --  ------------------------------
+   overriding
+   procedure Freeze (Node : access Choose_Tag_Node) is
+      Child  : Tag_Node_Access := Node.First_Child;
+      Choice : When_Tag_Node_Access := null;
+   begin
+      while Child /= null loop
+         if Child.all in Otherwise_Tag_Node'Class then
+            Node.Otherwise := Child;
+
+         elsif Child.all in When_Tag_Node'Class then
+            if Choice = null then
+               Node.Choices := When_Tag_Node (Child.all)'Access;
+            else
+               Choice.Next_Choice := When_Tag_Node (Child.all)'Access;
+            end if;
+            Choice := When_Tag_Node (Child.all)'Access;
+
+         else
+            null;
+            --  @todo: report a warning in a log
+            --  @todo: clean the sub-tree and remove what is not necessary
+         end if;
+         Child := Child.Next;
+      end loop;
+   end Freeze;
+
+   --  ------------------------------
+   --  Build the component tree from the tag node and attach it as
+   --  the last child of the given parent.  Calls recursively the
+   --  method to create children.
+   --  ------------------------------
+   overriding
+   procedure Build_Components (Node    : access Choose_Tag_Node;
+                               Parent  : in UIComponent_Access;
+                               Context : in out Facelet_Context'Class) is
+      Choice : When_Tag_Node_Access := Node.Choices;
+   begin
+      --  Evaluate the choices and stop at the first which succeeds
+      while Choice /= null loop
+         if Choice.Is_Selected (Context) then
+            Choice.Build_Children (Parent, Context);
+            return;
+         end if;
+         Choice := Choice.Next_Choice;
+      end loop;
+
+      --  No choice matched, build the otherwise clause.
+      if Node.Otherwise /= null then
+         Node.Otherwise.Build_Children (Parent, Context);
+      end if;
+   end Build_Components;
+
+   --  ------------------------------
+   --  Create the When Tag
+   --  ------------------------------
+   function Create_When_Tag_Node (Name       : Unbounded_String;
+                                  Parent     : Tag_Node_Access;
+                                  Attributes : Tag_Attribute_Array_Access)
+                                  return Tag_Node_Access is
+      Node : constant When_Tag_Node_Access := new When_Tag_Node;
+   begin
+      Node.Name       := Name;
+      Node.Parent     := Parent;
+      Node.Attributes := Attributes;
+      Node.Condition  := Find_Attribute (Attributes, "test");
+      --        Node.Var        := Find_Attribute (Attributes, "var");
+      return Node.all'Access;
+   end Create_When_Tag_Node;
+
+   --  ------------------------------
+   --  Check whether the node condition is selected.
+   --  ------------------------------
+   function Is_Selected (Node    : When_Tag_Node;
+                         Context : Facelet_Context'Class) return Boolean is
+   begin
+      return EL.Objects.To_Boolean (Get_Value (Node.Condition.all, Context));
+   end Is_Selected;
+
+   --  ------------------------------
+   --  Create the Otherwise Tag
+   --  ------------------------------
+   function Create_Otherwise_Tag_Node (Name       : Unbounded_String;
+                                       Parent     : Tag_Node_Access;
+                                       Attributes : Tag_Attribute_Array_Access)
+                                       return Tag_Node_Access is
+      Node : constant Otherwise_Tag_Node_Access := new Otherwise_Tag_Node;
+   begin
+      Node.Name       := Name;
+      Node.Parent     := Parent;
+      Node.Attributes := Attributes;
+      return Node.all'Access;
+   end Create_Otherwise_Tag_Node;
+
+   --  Tag names
+   CHOOSE_TAG    : aliased constant String := "choose";
+   IF_TAG        : aliased constant String := "if";
+   OTHERWISE_TAG : aliased constant String := "otherwise";
+   SET_TAG       : aliased constant String := "set";
+   WHEN_TAG      : aliased constant String := "when";
+
+   --  Name-space URI.  Use the JSTL name-space to make the XHTML views compatible
+   --  the JSF.
+   URI           : aliased constant String := "http://java.sun.com/jstl/core";
+
+   --  Tag library definition.  Names must be sorted.
+   Tag_Bindings  : aliased constant ASF.Factory.Binding_Array
+     := ((Name => CHOOSE_TAG'Access,
+          Component => null,
+          Tag => Create_Choose_Tag_Node'Access),
+       (Name => IF_TAG'Access,
+        Component => null,
+        Tag => Create_If_Tag_Node'Access),
+       (Name => OTHERWISE_TAG'Access,
+        Component => null,
+        Tag => Create_Otherwise_Tag_Node'Access),
+       (Name => SET_TAG'Access,
+        Component => null,
+        Tag => Create_Set_Tag_Node'Access),
+       (Name => WHEN_TAG'Access,
+        Component => null,
+        Tag => Create_When_Tag_Node'Access));
+
+   Tag_Factory   : aliased constant ASF.Factory.Factory_Bindings
+     := (URI => URI'Access, Bindings => Tag_Bindings'Access);
+
+   --  ------------------------------
+   --  Tag factory for nodes defined in this package.
+   --  ------------------------------
+   function Definition return ASF.Factory.Factory_Bindings_Access is
+   begin
+      return Tag_Factory'Access;
+   end Definition;
+
+end ASF.Views.Nodes.Core;
---
+++ src/asf-views-nodes-core.ads
@@ -0,0 +1,171 @@
+-----------------------------------------------------------------------
+--  nodes-core -- Core nodes
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+--  The <b>ASF.Views.Nodes.Core</b> package defines some pre-defined
+--  core tag nodes which are mapped in the following namespaces:
+--
+--    xmlns:c="http://java.sun.com/jstl/core"
+--    xmlns:ui="http://java.sun.com/jsf/facelets"
+--
+--  The following JSTL core elements are defined:
+--    <c:set var="name" value="#{expr}"/>
+--    <c:if test="#{expr}"> ...</c:if>
+--    <c:choose><c:when test="#{expr}"></c:when><c:otherwise/</c:choose>
+--
+--
+with ASF.Factory;
+package ASF.Views.Nodes.Core is
+
+   --  Tag factory for nodes defined in this package.
+   function Definition return ASF.Factory.Factory_Bindings_Access;
+
+   --  ------------------------------
+   --  Set Tag
+   --  ------------------------------
+   --  The <c:set var="name" value="#{expr}"/> variable creation.
+   --  The variable is created in the faces context.
+   type Set_Tag_Node is new Tag_Node with private;
+   type Set_Tag_Node_Access is access all Set_Tag_Node'Class;
+
+   --  Create the Set Tag
+   function Create_Set_Tag_Node (Name       : Unbounded_String;
+                                 Parent     : Tag_Node_Access;
+                                 Attributes : Tag_Attribute_Array_Access)
+                                 return Tag_Node_Access;
+
+   --  Build the component tree from the tag node and attach it as
+   --  the last child of the given parent.  Calls recursively the
+   --  method to create children.
+   overriding
+   procedure Build_Components (Node    : access Set_Tag_Node;
+                               Parent  : in UIComponent_Access;
+                               Context : in out Facelet_Context'Class);
+
+   --  ------------------------------
+   --  If Tag
+   --  ------------------------------
+   --  The <c:if test="#{expr}"> ...</c:if> condition.
+   --  If the condition evaluates to true when the component tree is built,
+   --  the children of this node are evaluated.  Otherwise the entire
+   --  sub-tree is not present in the component tree.
+   type If_Tag_Node is new Tag_Node with private;
+   type If_Tag_Node_Access is access all If_Tag_Node'Class;
+
+   --  Create the If Tag
+   function Create_If_Tag_Node (Name       : Unbounded_String;
+                                Parent     : Tag_Node_Access;
+                                Attributes : Tag_Attribute_Array_Access)
+                                return Tag_Node_Access;
+
+   --  Build the component tree from the tag node and attach it as
+   --  the last child of the given parent.  Calls recursively the
+   --  method to create children.
+   overriding
+   procedure Build_Components (Node    : access If_Tag_Node;
+                               Parent  : in UIComponent_Access;
+                               Context : in out Facelet_Context'Class);
+
+   --  ------------------------------
+   --  Choose Tag
+   --  ------------------------------
+   --  The <c:choose> ...</c:choose> choice.
+   --  Evaluate a set of choices (<c:when>) until one of them is found.
+   --  When no choice is found, evaluate the <c:otherwise> node.
+   type Choose_Tag_Node is new Tag_Node with private;
+   type Choose_Tag_Node_Access is access all Choose_Tag_Node'Class;
+
+   --  Freeze the tag node tree and perform any initialization steps
+   --  necessary to build the components efficiently.
+   --  Prepare the evaluation of choices by identifying the <c:when> and
+   --  <c:otherwise> conditions.
+   overriding
+   procedure Freeze (Node : access Choose_Tag_Node);
+
+   --  Build the component tree from the tag node and attach it as
+   --  the last child of the given parent.  Calls recursively the
+   --  method to create children.
+   overriding
+   procedure Build_Components (Node    : access Choose_Tag_Node;
+                               Parent  : in UIComponent_Access;
+                               Context : in out Facelet_Context'Class);
+
+   --  Create the <c:choose> tag node
+   function Create_Choose_Tag_Node (Name       : Unbounded_String;
+                                    Parent     : Tag_Node_Access;
+                                    Attributes : Tag_Attribute_Array_Access)
+                                   return Tag_Node_Access;
+
+   --  ------------------------------
+   --  When Tag
+   --  ------------------------------
+   --  The <c:when test="#{expr}"> ...</c:when> choice.
+   --  If the condition evaluates to true when the component tree is built,
+   --  the children of this node are evaluated.  Otherwise the entire
+   --  sub-tree is not present in the component tree.
+   type When_Tag_Node is new If_Tag_Node with private;
+   type When_Tag_Node_Access is access all When_Tag_Node'Class;
+
+   --  Check whether the node condition is selected.
+   function Is_Selected (Node    : When_Tag_Node;
+                         Context : Facelet_Context'Class) return Boolean;
+
+   --  Create the When Tag
+   function Create_When_Tag_Node (Name       : Unbounded_String;
+                                  Parent     : Tag_Node_Access;
+                                  Attributes : Tag_Attribute_Array_Access)
+                                 return Tag_Node_Access;
+
+   --  ------------------------------
+   --  Otherwise Tag
+   --  ------------------------------
+   --  The <c:otherwise> ...</c:otherwise> choice.
+   --  When all the choice conditions were false, the component tree is built,
+   --  the children of this node are evaluated.
+   type Otherwise_Tag_Node is new Tag_Node with null record;
+   type Otherwise_Tag_Node_Access is access all Otherwise_Tag_Node'Class;
+
+   --  Create the Otherwise Tag
+   function Create_Otherwise_Tag_Node (Name       : Unbounded_String;
+                                       Parent     : Tag_Node_Access;
+                                       Attributes : Tag_Attribute_Array_Access)
+                                       return Tag_Node_Access;
+
+   --  Java Facelet provides a <c:repeat> tag.  It must not be implemented
+   --  because it was proven this was not a good method for iterating over a list.
+private
+
+   type Set_Tag_Node is new Tag_Node with record
+      Var   : Tag_Attribute_Access;
+      Value : Tag_Attribute_Access;
+   end record;
+
+   type If_Tag_Node is new Tag_Node with record
+      Condition : Tag_Attribute_Access;
+      Var       : Tag_Attribute_Access;
+   end record;
+
+   type When_Tag_Node is new If_Tag_Node with record
+      Next_Choice : When_Tag_Node_Access;
+   end record;
+
+   type Choose_Tag_Node is new Tag_Node with record
+      Choices   : When_Tag_Node_Access;
+      Otherwise : Tag_Node_Access;
+   end record;
+
+end ASF.Views.Nodes.Core;

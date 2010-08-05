0d5c2c74abfd07ea6924867e02bf19df854053b8
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-08-05 21:00:29 +0000

    AWS requests

---
+++ src/aws/asf-requests-web.ads
@@ -0,0 +1,32 @@
+-----------------------------------------------------------------------
+--  asf.requests -- ASF Requests
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with AWS.Status;
+package ASF.Requests.Web is
+
+   type Request is new ASF.Requests.Request with private;
+   type Request_Access is access all Request'Class;
+
+   function Get_Parameter (R : Request; Name : String) return String;
+
+private
+
+   type Request is new ASF.Requests.Request with record
+      Data : access AWS.Status.Data;
+   end record;
+
+end ASF.Requests.Web;

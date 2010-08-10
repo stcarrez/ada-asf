00e66f79acf497e428b44ed806190693a0509348
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-08-10 13:47:03 +0000

    Add configuration properties to configure error pages Add a Get function that returns the configuration property as a boolean.

--- src/asf-applications.adb
+++ src/asf-applications.adb
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Util.Properties.Basic;
 package body ASF.Applications is
 
    --  ------------------------------
@@ -25,6 +25,16 @@ package body ASF.Applications is
                  Param : Config_Param) return String is
    begin
       return Self.Get (Param.Name.all, Param.Default.all);
+   end Get;
+
+   --  ------------------------------
+   --  Get the configuration parameter;
+   --  ------------------------------
+   function Get (Self  : Config;
+                 Param : Config_Param) return Boolean is
+      use Util.Properties.Basic.Boolean_Property;
+   begin
+      return Get (Self, Param.Name.all, Boolean'Value (Param.Default.all));
    end Get;
 
 end ASF.Applications;
--- src/asf-applications.ads
+++ src/asf-applications.ads
@@ -41,6 +41,16 @@ package ASF.Applications is
    VIEW_FILE_EXT       : aliased constant String := "view.file_ext";
    DEF_VIEW_FILE_EXT   : aliased constant String := ".xhtml";
 
+   --  Whether white spaces in XHTML files are ignored.
+   VIEW_IGNORE_WHITE_SPACES_PARAM : constant Config_Param;
+   VIEW_IGNORE_WHITE_SPACES       : aliased constant String := "view.ignore_white_spaces";
+   DEF_IGNORE_WHITE_SPACES        : aliased constant String := "false";
+
+   --  Whether the unknown tags are escaped in the output
+   VIEW_ESCAPE_UNKNOWN_TAGS_PARAM : constant Config_Param;
+   VIEW_ESCAPE_UNKNOWN_TAGS       : aliased constant String := "view.escape_unknown_tags";
+   DEF_ESCAPE_UNKNOWN_TAGS        : aliased constant String := "false";
+
    --  Extensions used by static pages
    VIEW_STATIC_EXT_PARAM : constant Config_Param;
    VIEW_STATIC_EXT       : aliased constant String := "view.static.ext";
@@ -51,9 +61,23 @@ package ASF.Applications is
    VIEW_STATIC_DIR       : aliased constant String := "view.static.dir";
    DEF_STATIC_DIR        : aliased constant String := "css,js,scripts,themes";
 
+   --  The 404 error page to render
+   ERROR_404_PARAM       : constant Config_Param;
+   ERROR_404_PAGE        : aliased constant String := "view.error.404";
+   DEF_ERROR_404         : aliased constant String := "errors/404";
+
+   --  The 500 error page to render
+   ERROR_500_PARAM       : constant Config_Param;
+   ERROR_500_PAGE        : aliased constant String := "view.error.500";
+   DEF_ERROR_500         : aliased constant String := "errors/500";
+
    --  Get the configuration parameter;
    function Get (Self  : Config;
                  Param : Config_Param) return String;
+
+   --  Get the configuration parameter;
+   function Get (Self  : Config;
+                 Param : Config_Param) return Boolean;
 
 private
 
@@ -73,11 +97,25 @@ private
    VIEW_FILE_EXT_PARAM : constant Config_Param := P '(Name    => VIEW_FILE_EXT'Access,
                                                       Default => DEF_VIEW_FILE_EXT'Access);
 
+   VIEW_IGNORE_WHITE_SPACES_PARAM : constant Config_Param
+     := P '(Name    => VIEW_IGNORE_WHITE_SPACES'Access,
+            Default => DEF_IGNORE_WHITE_SPACES'Access);
+
+   VIEW_ESCAPE_UNKNOWN_TAGS_PARAM : constant Config_Param
+     := P '(Name    => VIEW_ESCAPE_UNKNOWN_TAGS'Access,
+            Default => DEF_ESCAPE_UNKNOWN_TAGS'Access);
+
    VIEW_STATIC_EXT_PARAM : constant Config_Param := P '(Name    => VIEW_STATIC_EXT'Access,
                                                         Default => DEF_STATIC_EXT'Access);
 
    VIEW_STATIC_DIR_PARAM : constant Config_Param := P '(Name    => VIEW_STATIC_DIR'Access,
                                                         Default => DEF_STATIC_DIR'Access);
+
+   ERROR_404_PARAM : constant Config_Param := P '(Name    => ERROR_404_PAGE'Access,
+                                                  Default => DEF_ERROR_404'Access);
+
+   ERROR_500_PARAM : constant Config_Param := P '(Name    => ERROR_500_PAGE'Access,
+                                                  Default => DEF_ERROR_500'Access);
 
    type Config is new Util.Properties.Manager with null record;
 end ASF.Applications;

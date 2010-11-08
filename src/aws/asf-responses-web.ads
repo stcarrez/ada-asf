78af75a56f1ce5f83d3f39f44df560e79e25b8fe
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-11-08 21:19:23 +0000

    Response implementation for AWS

---
+++ src/aws/asf-responses-web.ads
@@ -0,0 +1,35 @@
+-----------------------------------------------------------------------
+--  asf.requests -- ASF Requests
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with AWS.Status;
+with AWS.Response;
+package ASF.Responses.Web is
+
+   type Response is new ASF.Responses.Response with private;
+--     type Request_Access is access all Request'Class;
+
+--     function Get_Parameter (R : Request; Name : String) return String;
+
+   function Get_Data (Resp : in Response) return AWS.Response.Data;
+
+private
+
+   type Response is new ASF.Responses.Response with record
+      Data : AWS.Response.Data;
+   end record;
+
+end ASF.Responses.Web;

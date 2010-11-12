0d589febecba6555904afd3b5991bba3d89fa0ae
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-11-12 14:26:21 +0000

    Provide the ASF faces servlet - handle the Do_Get and give the request to the application dispatch

---
+++ src/asf-servlets-faces.ads
@@ -0,0 +1,112 @@
+-----------------------------------------------------------------------
+--  asf.servlets.faces -- Faces servlet
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Applications.Main;
+
+package ASF.Servlets.Faces is
+
+   --  The <b>Servlet</b> represents the component that will handle
+   --  an HTTP request received by the server.
+   type Faces_Servlet is new Servlet with private;
+
+   --  Called by the servlet container to indicate to a servlet that the servlet
+   --  is being placed into service.
+   overriding
+   procedure Initialize (Server  : in out Faces_Servlet;
+                         Context : in Servlet_Registry'Class);
+
+   --  Called by the server (via the service method) to allow a servlet to handle
+   --  a GET request.
+   --
+   --  Overriding this method to support a GET request also automatically supports
+   --  an HTTP HEAD request. A HEAD request is a GET request that returns no body
+   --  in the response, only the request header fields.
+   --
+   --  When overriding this method, read the request data, write the response headers,
+   --  get the response's writer or output stream object, and finally, write the
+   --  response data. It's best to include content type and encoding.
+   --  When using a PrintWriter object to return the response, set the content type
+   --  before accessing the PrintWriter object.
+   --
+   --  The servlet container must write the headers before committing the response,
+   --  because in HTTP the headers must be sent before the response body.
+   --
+   --  Where possible, set the Content-Length header (with the
+   --  Response.Set_Content_Length method), to allow the servlet container
+   --  to use a persistent connection to return its response to the client,
+   --  improving performance. The content length is automatically set if the entire
+   --  response fits inside the response buffer.
+   --
+   --  When using HTTP 1.1 chunked encoding (which means that the response has a
+   --  Transfer-Encoding header), do not set the Content-Length header.
+   --
+   --  The GET method should be safe, that is, without any side effects for which
+   --  users are held responsible. For example, most form queries have no side effects.
+   --  If a client request is intended to change stored data, the request should use
+   --  some other HTTP method.
+   --
+   --  The GET method should also be idempotent, meaning that it can be safely repeated.
+   --  Sometimes making a method safe also makes it idempotent. For example, repeating
+   --  queries is both safe and idempotent, but buying a product online or modifying
+   --  data is neither safe nor idempotent.
+   --
+   --  If the request is incorrectly formatted, Do_Get  returns an HTTP "Bad Request"
+   procedure Do_Get (Server   : in Faces_Servlet;
+                     Request  : in out Requests.Request'Class;
+                     Response : in out Responses.Response'Class);
+
+   --  Called by the server (via the service method) to allow a servlet to handle
+   --  a POST request. The HTTP POST method allows the client to send data of unlimited
+   --  length to the Web server a single time and is useful when posting information
+   --  such as credit card numbers.
+   --
+   --  When overriding this method, read the request data, write the response headers,
+   --  get the response's writer or output stream object, and finally, write the
+   --  response data. It's best to include content type and encoding. When using
+   --  a PrintWriter object to return the response, set the content type before
+   --  accessing the PrintWriter object.
+   --
+   --  The servlet container must write the headers before committing the response,
+   --  because in HTTP the headers must be sent before the response body.
+   --
+   --  Where possible, set the Content-Length header (with the
+   --  Response.Set_Content_Length method), to allow the servlet container to use
+   --  a persistent connection to return its response to the client, improving
+   --  performance. The content length is automatically set if the entire response
+   --  fits inside the response buffer.
+   --
+   --  When using HTTP 1.1 chunked encoding (which means that the response has a
+   --  Transfer-Encoding header), do not set the Content-Length header.
+   --
+   --  This method does not need to be either safe or idempotent. Operations
+   --  requested through POST can have side effects for which the user can be held
+   --  accountable, for example, updating stored data or buying items online.
+   --
+   --  If the HTTP POST request is incorrectly formatted, doPost returns
+   --  an HTTP "Bad Request" message.
+   procedure Do_Post (Server   : in Faces_Servlet;
+                      Request  : in out Requests.Request'Class;
+                      Response : in out Responses.Response'Class);
+
+private
+
+   type Faces_Servlet is new Servlet with record
+      App    : ASF.Applications.Main.Application_Access;
+   end record;
+
+end ASF.Servlets.Faces;

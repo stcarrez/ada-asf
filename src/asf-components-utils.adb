f0e8392273d08f6316ee2db14b4438b29da919a7
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-11-16 12:07:01 +0000

    Implement the Get_Line_Info function

---
+++ src/asf-components-utils.adb
@@ -0,0 +1,45 @@
+-----------------------------------------------------------------------
+--  components-util -- ASF Util Components
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with ASF.Views.Nodes;
+package body ASF.Components.Utils is
+
+   --  ------------------------------
+   --  Get the line information where the component is defined.
+   --  ------------------------------
+   function Get_Line_Info (UI : in UIComponent'Class) return Views.Nodes.Line_Info is
+   begin
+      if UI.Tag /= null then
+         return UI.Tag.Get_Line_Info;
+      else
+         return UI.Tag.Get_Line_Info;
+      end if;
+   end Get_Line_Info;
+
+   --  ------------------------------
+   --  Get the line information where the component is defined.
+   --  ------------------------------
+   function Get_Line_Info (UI : in UIComponent'Class) return String is
+   begin
+      if UI.Tag /= null then
+         return UI.Tag.Get_Line_Info;
+      else
+         return UI.Tag.Get_Line_Info;
+      end if;
+   end Get_Line_Info;
+
+end ASF.Components.Utils;

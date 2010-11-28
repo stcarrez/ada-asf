094362bf66b6fc8b34fdfe6a8d6471dbe93d886c
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-11-28 22:36:32 +0000

    Implement the restore process as a separate phase controller

---
+++ src/asf-lifecycles-restore.adb
@@ -0,0 +1,62 @@
+-----------------------------------------------------------------------
+--  asf-lifecycles-restore -- Restore view phase
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Exceptions;
+with ASF.Applications.Main;
+with ASF.Applications.Views;
+with ASF.Components.Root;
+with ASF.Requests;
+with Util.Log.Loggers;
+package body ASF.Lifecycles.Restore is
+
+   use Ada.Exceptions;
+   use Util.Log;
+   use ASF.Applications;
+
+   --  The logger
+   Log : constant Loggers.Logger := Loggers.Create ("ASF.Lifecycles.Restore");
+
+   --  ------------------------------
+   --  Execute the restore view phase.
+   --  ------------------------------
+   overriding
+   procedure Execute (Controller : in Restore_Controller;
+                      Context    : in out ASF.Contexts.Faces.Faces_Context'Class) is
+      pragma Unreferenced (Controller);
+
+      Req     : constant ASF.Requests.Request_Access := Context.Get_Request;
+      Page    : constant String := Req.Get_Path_Info;
+      App     : constant access Main.Application'Class   := Context.Get_Application;
+      Handler : constant access Views.View_Handler'Class := App.Get_View_Handler;
+      View    : Components.Root.UIViewRoot;
+   begin
+      Handler.Restore_View (Page, Context, View);
+
+      Context.Set_View_Root (View);
+
+      if Req.Get_Method = "GET" then
+         Context.Response_Completed;
+      end if;
+
+   exception
+      when E : others =>
+         Log.Error ("Error when restoring view {0}: {1}: {2}", Page,
+                    Exception_Name (E), Exception_Message (E));
+         raise;
+   end Execute;
+
+end ASF.Lifecycles.Restore;

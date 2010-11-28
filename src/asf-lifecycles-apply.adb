ce8ef0de027f376d32596d6444944829af54d8bc
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-11-28 22:37:18 +0000

    Implement the apply values phase as a phase controller

---
+++ src/asf-lifecycles-apply.adb
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------
+--  asf-lifecycles-apply -- Apply values phase
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Exceptions;
+with ASF.Components.Root;
+with ASF.Components.Base;
+with Util.Log.Loggers;
+
+--  The <b>ASF.Lifecycles.Apply</b> package defines the behavior
+--  of the apply values phase.
+package body ASF.Lifecycles.Apply is
+
+   use Ada.Exceptions;
+   use Util.Log;
+   use ASF.Components;
+
+   --  The logger
+   Log : constant Loggers.Logger := Loggers.Create ("ASF.Lifecycles.Response");
+
+   --  ------------------------------
+   --  Execute the apply values phase.
+   --  ------------------------------
+   overriding
+   procedure Execute (Controller : in Apply_Controller;
+                      Context    : in out ASF.Contexts.Faces.Faces_Context'Class) is
+      pragma Unreferenced (Controller);
+
+      View : constant Components.Root.UIViewRoot := Context.Get_View_Root;
+      Root : constant access Components.Base.UIComponent'Class := Components.Root.Get_Root (View);
+   begin
+      Root.Process_Decodes (Context);
+
+   exception
+      when E: others =>
+         Log.Error ("Error when applying values on view {0}: {1}: {2}", "?",
+                    Exception_Name (E), Exception_Message (E));
+         raise;
+   end Execute;
+
+end ASF.Lifecycles.Apply;

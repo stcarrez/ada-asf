395e51646dabe4c14bd7d7d77ac2de614118f24f
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-11-29 22:37:34 +0000

    Provide a default lifecycle handler with the default phase controllers

---
+++ src/asf-lifecycles-default.adb
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------
+--  asf-lifecycles-default -- Default Lifecycle handler
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Lifecycles.Restore;
+with ASF.Lifecycles.Apply;
+with ASF.Lifecycles.Response;
+package body ASF.Lifecycles.Default is
+
+   --  ------------------------------
+   --  Creates the phase controllers by invoking the <b>Set_Controller</b>
+   --  procedure for each phase.  This is called by <b>Initialize</b> to build
+   --  the lifecycle handler.
+   --  ------------------------------
+   procedure Create_Phase_Controllers (Controller : in out Lifecycle) is
+   begin
+      Controller.Set_Controller (Phase    => RESTORE_VIEW,
+                                 Instance => new Lifecycles.Restore.Restore_Controller);
+
+      Controller.Set_Controller (Phase    => APPLY_REQUEST_VALUES,
+                                 Instance => new Lifecycles.Apply.Apply_Controller);
+
+      Controller.Set_Controller (Phase    => RENDER_RESPONSE,
+                                 Instance => new Lifecycles.Response.Response_Controller);
+   end Create_Phase_Controllers;
+
+   --  ------------------------------
+   --  Initialize the the lifecycle handler.
+   --  ------------------------------
+   overriding
+   procedure Initialize (Controller : in out Lifecycle;
+                         App        : access ASF.Applications.Main.Application'Class) is
+   begin
+      Lifecycle'Class (Controller).Create_Phase_Controllers;
+      for Phase in Controller.Controllers'Range loop
+         Controller.Controllers (Phase).Initialize (App);
+      end loop;
+   end Initialize;
+
+end ASF.Lifecycles.Default;
---
+++ src/asf-lifecycles-default.ads
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------
+--  asf-lifecycles-default -- Default Lifecycle handler
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package ASF.Lifecycles.Default is
+
+   type Lifecycle is new ASF.Lifecycles.Lifecycle with null record;
+
+   --  Creates the phase controllers by invoking the <b>Set_Controller</b>
+   --  procedure for each phase.  This is called by <b>Initialize</b> to build
+   --  the lifecycle handler.
+   procedure Create_Phase_Controllers (Controller : in out Lifecycle);
+
+   --  Initialize the the lifecycle handler.
+   overriding
+   procedure Initialize (Controller : in out Lifecycle;
+                         App        : access ASF.Applications.Main.Application'Class);
+
+end ASF.Lifecycles.Default;

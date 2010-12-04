6f0392974eeea3c6421c7af6fea79e5b87b21f86
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-12-04 15:38:46 +0000

    Defines the Faces_Event type with basic operations on the event (See JSR 314 - 3.4.2 Application Events)

---
+++ src/asf-events.adb
@@ -0,0 +1,46 @@
+-----------------------------------------------------------------------
+--  asf-events -- ASF Events
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package body ASF.Events is
+
+   --  ------------------------------
+   --  Get the lifecycle phase where the event must be processed.
+   --  ------------------------------
+   function Get_Phase (Event : in Faces_Event) return ASF.Lifecycles.Phase_Type is
+   begin
+      return Event.Phase;
+   end Get_Phase;
+
+   --  ------------------------------
+   --  Set the lifecycle phase when this event must be processed.
+   --  ------------------------------
+   procedure Set_Phase (Event : in out Faces_Event;
+                        Phase : in ASF.Lifecycles.Phase_Type) is
+   begin
+      Event.Phase := Phase;
+   end Set_Phase;
+
+   --  ------------------------------
+   --  Get the component onto which the event was posted.
+   --  ------------------------------
+   function Get_Component (Event : in Faces_Event) return Components.Base.UIComponent_Access is
+   begin
+      return Event.Component;
+   end Get_Component;
+
+end ASF.Events;
--- src/asf-events.ads
+++ src/asf-events.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  asf-events -- ASF Events
---  Copyright (C) 2009, 2010 Stephane Carrez
+--  Copyright (C) 2010 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,42 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Util.Events;
+with ASF.Lifecycles;
+with ASF.Components.Base;
 
+--  The <b>ASF.Events</b> package defines the application events that an ASF
+--  application can receive.  Events are queued while processing the JSF phases
+--  (See UIComponent.Queue_Event).  They are dispatched after each phase
+--  (See UIComponent.Broadcast).
+--
+--  This package is an Ada adaptation for the Java Server Faces Specification
+--  JSR 314 - 3.4.2 Application Events.
 package ASF.Events is
+
+   --  ------------------------------
+   --  Faces event
+   --  ------------------------------
+   --  The <b>Faces_Event</b> represents the root type for ASF events.
+   --  The event is associated with a component and a lifecycle phase after
+   --  which it will be processed.
+   type Faces_Event is new Util.Events.Event with private;
+
+   --  Get the lifecycle phase where the event must be processed.
+   function Get_Phase (Event : in Faces_Event) return ASF.Lifecycles.Phase_Type;
+
+   --  Set the lifecycle phase when this event must be processed.
+   procedure Set_Phase (Event : in out Faces_Event;
+                        Phase : in ASF.Lifecycles.Phase_Type);
+
+   --  Get the component onto which the event was posted.
+   function Get_Component (Event : in Faces_Event) return Components.Base.UIComponent_Access;
+
+private
+
+   type Faces_Event is new Util.Events.Event with record
+      Phase     : ASF.Lifecycles.Phase_Type := ASF.Lifecycles.RESTORE_VIEW;
+      Component : Components.Base.UIComponent_Access := null;
+   end record;
 
 end ASF.Events;

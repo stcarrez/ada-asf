8d49e832033e3aaee911d8eda6fec860f7c09aff
Stephane Carrez <Stephane.Carrez@gmail.com>
2010-12-05 09:30:36 +0000

    Define the Action_Event and the Action_Listener

---
+++ src/asf-events-actions.adb
@@ -0,0 +1,45 @@
+-----------------------------------------------------------------------
+--  asf-events -- ASF Events
+--  Copyright (C) 2009, 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Util.Events;
+with ASF.Lifecycles;
+with ASF.Contexts.Faces;
+with ASF.Components.Base;
+package body ASF.Events.Actions is
+
+   --  ------------------------------
+   --  Get the method expression to invoke
+   --  ------------------------------
+   function Get_Method (Event : in Action_Event) return EL.Expressions.Method_Expression is
+   begin
+      return Event.Method;
+   end Get_Method;
+
+   --  ------------------------------
+   --  Post an <b>Action_Event</b> on the component.
+   --  ------------------------------
+   procedure Post_Event (UI     : in out Components.Base.UIComponent'Class;
+                         Method : in EL.Expressions.Method_Expression) is
+      Ev : Action_Event_Access := new Action_Event;
+   begin
+      Ev.Phase     := ASF.Lifecycles.INVOKE_APPLICATION;
+      Ev.Component := UI'Unchecked_Access;
+      Ev.Method    := Method;
+      UI.Queue_Event(Ev.all'Access);
+   end Post_Event;
+
+end ASF.Events.Actions;
---
+++ src/asf-events-actions.ads
@@ -0,0 +1,63 @@
+-----------------------------------------------------------------------
+--  asf-events-actions -- Actions Events
+--  Copyright (C) 2010 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+with EL.Expressions;
+with EL.Beans.Methods.Proc_1;
+with ASF.Contexts.Faces;
+with ASF.Components.Base;
+package ASF.Events.Actions is
+
+   package Action_Method is
+     new EL.Beans.Methods.Proc_1 (Param1_Type => Ada.Strings.Unbounded.Unbounded_String);
+
+   --  ------------------------------
+   --  Action event
+   --  ------------------------------
+   --  The <b>Action_Event</b> is posted when a method bean action must be executed
+   --  as a result of a command (such as button press).  The method action must return
+   --  an outcome string that indicates which view to return.
+   type Action_Event is new Faces_Event with private;
+   type Action_Event_Access is access all Action_Event'Class;
+
+   --  Get the method expression to invoke
+   function Get_Method (Event : in Action_Event) return EL.Expressions.Method_Expression;
+
+   --  Post an <b>Action_Event</b> on the component.
+   procedure Post_Event (UI     : in out Components.Base.UIComponent'Class;
+                         Method : in EL.Expressions.Method_Expression);
+
+   --  ------------------------------
+   --  Action Listener
+   --  ------------------------------
+   --  The <b>Action_Listener</b> is the interface to receive the <b>Action_Event</b>.
+   type Action_Listener is limited interface and Util.Events.Event_Listener;
+   type Action_Listener_Access is access all Action_Listener'Class;
+
+   --  Process the action associated with the action event.
+   procedure Process_Action (Listener : in Action_Listener;
+                             Event    : in Action_Event'Class;
+                             Context  : in out Contexts.Faces.Faces_Context'Class) is abstract;
+
+private
+
+   type Action_Event is new Faces_Event with record
+      Method : EL.Expressions.Method_Expression;
+   end record;
+
+end ASF.Events.Actions;

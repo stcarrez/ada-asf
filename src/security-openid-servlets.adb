e609577f0aa1183369b0485d9bda11638fcee925
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-04-30 21:16:19 +0000

    Servlets for OpenID authentication

---
+++ src/security-openid-servlets.adb
@@ -0,0 +1,166 @@
+-----------------------------------------------------------------------
+--  security-openid-servlets - Servlets for OpenID 2.0 Authentication
+--  Copyright (C) 2010, 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Sessions;
+
+with Util.Beans.Objects;
+with Util.Beans.Objects.Records;
+with Util.Log.Loggers;
+package body Security.Openid.Servlets is
+
+   use Util.Log;
+
+   --  The logger
+   Log : constant Loggers.Logger := Loggers.Create ("Security.Openid.Servlets");
+
+   --  Make a package to store the Association in the session.
+   package Association_Bean is new Util.Beans.Objects.Records (Security.Openid.Association);
+
+   subtype Association_Access is Association_Bean.Element_Type_Access;
+
+   --  ------------------------------
+   --  Called by the servlet container to indicate to a servlet that the servlet
+   --  is being placed into service.
+   --  ------------------------------
+   procedure Initialize (Server  : in out Openid_Servlet;
+                         Context : in ASF.Servlets.Servlet_Registry'Class) is
+   begin
+      null;
+   end Initialize;
+
+   --  Property name that specifies the OpenID callback URL.
+   OPENID_VERIFY_URL      : constant String := "openid.callback_url";
+
+   --  Property name that specifies the realm.
+   OPENID_REALM           : constant String := "openid.realm";
+
+   --  Name of the session attribute which holds information about the active authentication.
+   OPENID_ASSOC_ATTRIBUTE : constant String := "openid-assoc";
+
+   procedure Initialize (Server  : in Openid_Servlet;
+                         Manager : in out Security.Openid.Manager) is
+      Ctx          : constant ASF.Servlets.Servlet_Registry_Access := Server.Get_Servlet_Context;
+      Callback_URI : constant String := Ctx.Get_Init_Parameter (OPENID_VERIFY_URL);
+      Realm        : constant String := Ctx.Get_Init_Parameter (OPENID_REALM);
+   begin
+      Manager.Initialize (Return_To => Callback_URI,
+                          Name      => Realm);
+   end Initialize;
+
+   function Get_Provider_URL (Server   : in Request_Auth_Servlet;
+                              Request  : in ASF.Requests.Request'Class) return String is
+      Ctx  : constant ASF.Servlets.Servlet_Registry_Access := Server.Get_Servlet_Context;
+      URI  : constant String := Request.Get_Path_Info;
+   begin
+      if URI'Length = 0 then
+         return "";
+      end if;
+      Log.Info ("OpenID authentication with {0}", URI);
+      return Ctx.Get_Init_Parameter ("openid.provider." & URI (URI'First + 1 .. URI'Last));
+   end Get_Provider_URL;
+
+   --  ------------------------------
+   --  Proceed to the OpenID authentication with an OpenID provider.
+   --  Find the OpenID provider URL and starts the discovery, association phases
+   --  during which a private key is obtained from the OpenID provider.
+   --  After OpenID discovery and association, the user will be redirected to
+   --  the OpenID provider.
+   --  ------------------------------
+   procedure Do_Get (Server   : in Request_Auth_Servlet;
+                     Request  : in out ASF.Requests.Request'Class;
+                     Response : in out ASF.Responses.Response'Class) is
+
+      Provider : constant String := Server.Get_Provider_URL (Request);
+   begin
+      Log.Info ("Request OpenId authentication to {0}", Provider);
+
+      if Provider'Length = 0 then
+         Response.Set_Status (ASF.Responses.SC_NOT_FOUND);
+         return;
+      end if;
+
+      declare
+         Mgr   : Security.Openid.Manager;
+         OP    : Security.Openid.End_Point;
+         Bean  : constant Util.Beans.Objects.Object := Association_Bean.Create;
+         Assoc : constant Association_Access := Association_Bean.To_Element_Access (Bean);
+      begin
+         Server.Initialize (Mgr);
+
+         --  Yadis discovery (get the XRDS file).
+         Mgr.Discover (Provider, OP);
+
+         --  Associate to the OpenID provider and get an end-point with a key.
+         Mgr.Associate (OP, Assoc.all);
+
+         --  Save the association in the HTTP session and
+         --  redirect the user to the OpenID provider.
+         declare
+            Auth_URL : constant String := Mgr.Get_Authentication_URL (OP, Assoc.all);
+            Session  : ASF.Sessions.Session := Request.Get_Session (Create => True);
+         begin
+            Log.Info ("Redirect to auth URL: {0}", Auth_URL);
+
+            Response.Send_Redirect (Location => Auth_URL);
+            Session.Set_Attribute (Name  => OPENID_ASSOC_ATTRIBUTE,
+                                   Value => Bean);
+         end;
+      end;
+   end Do_Get;
+
+   --  ------------------------------
+   --  Verify the authentication result that was returned by the OpenID provider.
+   --  If the authentication succeeded and the signature was correct, sets a
+   --  user principals on the session.
+   --  ------------------------------
+   procedure Do_Get (Server   : in Verify_Auth_Servlet;
+                     Request  : in out ASF.Requests.Request'Class;
+                     Response : in out ASF.Responses.Response'Class) is
+
+      Session : ASF.Sessions.Session := Request.Get_Session (Create => False);
+      Bean    : Util.Beans.Objects.Object := Session.Get_Attribute (OPENID_ASSOC_ATTRIBUTE);
+      Mgr     : Security.Openid.Manager;
+      OP      : Security.Openid.End_Point;
+      Assoc   : Association_Access;
+      Auth    : Security.Openid.Authentication;
+   begin
+      Log.Info ("Verify openid authentication");
+
+      --  Cleanup the session and drop the association end point.
+      Session.Remove_Attribute (OPENID_ASSOC_ATTRIBUTE);
+      if Util.Beans.Objects.Is_Null (Bean) then
+         Log.Warn ("Verify openid request without active session");
+         Response.Set_Status (ASF.Responses.SC_FORBIDDEN);
+         return;
+      end if;
+
+      Assoc := Association_Bean.To_Element_Access (Bean);
+      Server.Initialize (Mgr);
+
+      --  Verify that what we receive through the callback matches the association key.
+      Mgr.Verify (Assoc.all, Request, Auth);
+      if Get_Status (Auth) /= AUTHENTICATED then
+         Log.Info ("Authentication has failed");
+         Response.Set_Status (ASF.Responses.SC_FORBIDDEN);
+         return;
+      end if;
+
+      Log.Info ("Authentication succeeded for {0}", Get_Email (Auth));
+   end Do_Get;
+
+end Security.Openid.Servlets;
---
+++ src/security-openid-servlets.ads
@@ -0,0 +1,123 @@
+-----------------------------------------------------------------------
+--  security-openid-servlets - Servlets for OpenID 2.0 Authentication
+--  Copyright (C) 2010, 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Servlets;
+with ASF.Requests;
+with ASF.Responses;
+
+--  The <b>Security.Openid.Servlets</b> package defines two servlets that can be used
+--  to implement an OpenID 2.0 authentication with an OpenID provider such as Google,
+--  Yahoo!, Verisign, Orange, ...
+--
+--  The process to use these servlets is the following:
+--  <ul>
+--    <li>Declare and register a <b>Request_Auth_Servlet</b> servlet.</li>
+--    <li>Map that servlet to an authenticate URL.  To authenticate, a user will have
+--        to click on a link mapped to that servlet.  To identify the OpenID provider,
+--        the URL must contain the name of the provider.  Example:
+--
+--           http://localhost:8080/app/openid/auth/google
+--
+--    <li>Declare and register a <b>Verify_Auth_Servlet</b> servlet.
+--    <li>Map that servlet to the verification URL.  This is the URL that the user
+--        will return to when authentication is done by the OpenID provider.  Example:
+--
+--           http://localhost:8080/app/openid/verify
+--
+--    <li>Declare in the application properties a set of configurations:
+--        <ul>
+--            <li>The <b>openid.realm</b> must indicate the URL that identifies the
+--                application the end user will trust.  Example:
+--
+--                    openid.realm=http://localhost:8080/app
+--
+--            <li>The <b>openid.callback_url</b> must indicate the callback URL to
+--                which the OpenID provider will redirect the user when authentication
+--                is finished (successfully or not).  The callback URL must match
+--                the realm.
+--
+--                    openid.callback_url=http://localhost:8080/app/openid/verify
+--
+--            <li>For each OpenID provider, a URL to the Yadis entry point of the
+--                OpenID provider is necessary.  This URL is used to get the XRDS
+--                stream giving endoint of the OpenID provider.  Example:
+--
+--                    openid.provider.google=https://www.google.com/accounts/o8/id
+--        </ul>
+--
+--  </ul>
+--
+package Security.Openid.Servlets is
+
+   --  ------------------------------
+   --  OpenID Servlet
+   --  ------------------------------
+   --  The <b>Openid_Servlet</b> is the OpenID root servlet for OpenID 2.0 authentication.
+   --  It is defined to provide a common basis for the authentication and verification servlets.
+   type Openid_Servlet is abstract new ASF.Servlets.Servlet with private;
+
+   --  Called by the servlet container to indicate to a servlet that the servlet
+   --  is being placed into service.
+   procedure Initialize (Server  : in out Openid_Servlet;
+                         Context : in ASF.Servlets.Servlet_Registry'Class);
+
+   --  ------------------------------
+   --  OpenID Request Servlet
+   --  ------------------------------
+   --  The <b>Request_Auth_Servlet</b> servlet implements the first steps of an OpenID
+   --  authentication.
+   type Request_Auth_Servlet is new Openid_Servlet with private;
+
+   --  Proceed to the OpenID authentication with an OpenID provider.
+   --  Find the OpenID provider URL and starts the discovery, association phases
+   --  during which a private key is obtained from the OpenID provider.
+   --  After OpenID discovery and association, the user will be redirected to
+   --  the OpenID provider.
+   procedure Do_Get (Server   : in Request_Auth_Servlet;
+                     Request  : in out ASF.Requests.Request'Class;
+                     Response : in out ASF.Responses.Response'Class);
+
+   --  ------------------------------
+   --  OpenID Verification Servlet
+   --  ------------------------------
+   --  The <b>Verify_Auth_Servlet</b> verifies the authentication result and
+   --  extract authentication from the callback URL.
+   type Verify_Auth_Servlet is new Openid_Servlet with private;
+
+   --  Verify the authentication result that was returned by the OpenID provider.
+   --  If the authentication succeeded and the signature was correct, sets a
+   --  user principals on the session.
+   procedure Do_Get (Server   : in Verify_Auth_Servlet;
+                     Request  : in out ASF.Requests.Request'Class;
+                     Response : in out ASF.Responses.Response'Class);
+
+private
+   function Get_Provider_URL (Server   : in Request_Auth_Servlet;
+                              Request  : in ASF.Requests.Request'Class) return String;
+
+   procedure Initialize (Server  : in Openid_Servlet;
+                         Manager : in out Security.Openid.Manager);
+
+   type Openid_Servlet is new ASF.Servlets.Servlet with record
+      Manager : Security.Openid.Manager;
+   end record;
+
+   type Request_Auth_Servlet is new Openid_Servlet with null record;
+   type Verify_Auth_Servlet is new Openid_Servlet with null record;
+
+end Security.Openid.Servlets;

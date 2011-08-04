3f34d73c8adb38884e32ea059333175c374df55e
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-08-04 21:48:59 +0000

    Servlet mapping improvement - Allow to write extension mappings with path components   Example: /users/*.html

--- src/asf-servlets.adb
+++ src/asf-servlets.adb
@@ -730,6 +730,7 @@ package body ASF.Servlets is
       Prev_Node : Mapping_Access;
       Is_Last   : Boolean := False;
       Is_Wildcard : Boolean := False;
+      Is_Extension : Boolean := False;
       URI       : String_Access;
       Map       : Mapping_Access;
    begin
@@ -772,6 +773,8 @@ package body ASF.Servlets is
             Last_Pos    := Pattern'Last;
             Is_Last     := True;
             Is_Wildcard := Pattern (First_Pos .. Last_Pos) = "*";
+            Is_Extension := Pattern (First_Pos) = '*' and then Last_Pos >= First_Pos + 1
+              and then Pattern (First_Pos + 1) = '.';
          end if;
 
          --  Look for the node that matches
@@ -809,6 +812,8 @@ package body ASF.Servlets is
             Last_Pos    := Pattern'Last;
             Is_Last     := True;
             Is_Wildcard := Pattern (First_Pos .. Last_Pos) = "*";
+            Is_Extension := Pattern (First_Pos) = '*' and then Last_Pos >= First_Pos + 1
+              and then Pattern (First_Pos + 1) = '.';
          end if;
 
          URI := new String '(Pattern (First_Pos .. Last_Pos));
@@ -821,6 +826,17 @@ package body ASF.Servlets is
                                         Servlet          => Server,
                                         Next_Servlet_Map => Server.Mappings,
                                         Path_Pos         => First_Pos - 1,
+                                        Next_Map         => null);
+
+         elsif Is_Extension then
+            Node :=  new Mapping_Node '(Limited_Controlled with
+                                        URI              => URI,
+                                        Map_Type         => MAP_EXTENSION,
+                                        Child_Map        => null,
+                                        Filters          => null,
+                                        Servlet          => Server,
+                                        Next_Servlet_Map => Server.Mappings,
+                                        Path_Pos         => Last_Pos,
                                         Next_Map         => null);
 
          elsif Is_Last then
@@ -922,8 +938,13 @@ package body ASF.Servlets is
                when MAP_WILDCARD =>
                   return Node;
 
-               when others =>
-                  null;
+               --  We have an extension
+               when MAP_EXTENSION =>
+                  Pos  := Util.Strings.Rindex (Source => URI, Ch => '.');
+                  if Pos > 0 and then
+                    Node.URI (Node.URI'First + 1 .. Node.URI'Last) = URI (Pos .. URI'Last) then
+                     return Node;
+                  end if;
 
             end case;
             Node := Node.Next_Map;

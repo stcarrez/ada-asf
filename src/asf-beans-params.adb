43b2b5b9aa1570e074e5db90458651cc65dd8598
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-10-09 15:20:42 +0000

    Implement the 'request' pre-defined bean - Look for the request in the current ASF faces context

---
+++ src/asf-beans-params.adb
@@ -0,0 +1,50 @@
+-----------------------------------------------------------------------
+--  asf-beans-params -- Bean giving access to the request parameters
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Contexts.Faces;
+with ASF.Requests;
+package body ASF.Beans.Params is
+
+   --  ------------------------------
+   --  Get the request parameter identified by the given name.
+   --  Returns Null_Object if the request does not define such parameter.
+   --  ------------------------------
+   overriding
+   function Get_Value (Bean : in Param_Bean;
+                       Name : in String) return Util.Beans.Objects.Object is
+      pragma Unreferenced (Bean);
+      use type ASF.Contexts.Faces.Faces_Context_Access;
+
+      Ctx : constant ASF.Contexts.Faces.Faces_Context_Access := ASF.Contexts.Faces.Current;
+   begin
+      if Ctx = null then
+         return Util.Beans.Objects.Null_Object;
+      end if;
+      declare
+         Req   : constant ASF.Requests.Request_Access := Ctx.Get_Request;
+         Param : constant String := Req.Get_Parameter (Name);
+      begin
+         if Param = "" then
+            return Util.Beans.Objects.Null_Object;
+         else
+            return Util.Beans.Objects.To_Object (Param);
+         end if;
+      end;
+   end Get_Value;
+
+end ASF.Beans.Params;
---
+++ src/asf-beans-params.ads
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------
+--  asf-beans-params -- Bean giving access to the request parameters
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Beans.Basic;
+with Util.Beans.Objects;
+
+package ASF.Beans.Params is
+
+   --  ------------------------------
+   --  Request Parameter Bean
+   --  ------------------------------
+   --  The <b>Param_Bean</b> gives access to the request parameter.
+   --  The bean instance is global to the application.
+   type Param_Bean is new Util.Beans.Basic.Readonly_Bean with private;
+
+   --  Get the request parameter identified by the given name.
+   --  Returns Null_Object if the request does not define such parameter.
+   overriding
+   function Get_Value (Bean : in Param_Bean;
+                       Name : in String) return Util.Beans.Objects.Object;
+
+private
+
+   type Param_Bean is new Util.Beans.Basic.Readonly_Bean with null record;
+
+end ASF.Beans.Params;

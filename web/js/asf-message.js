df17c91832f0f7f52dccb83d29cd8d8324535983
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-01-27 17:58:44 +0000

    Add support for popup message

---
+++ web/js/asf-message.js
@@ -0,0 +1,70 @@
+/*
+ *  asf-message -- Message component
+ *  Copyright (C) 2013 Stephane Carrez
+ *  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+(function($, undefined) {
+
+    /**
+     * The message component is a small popup element intended to display some message.
+     */
+    $.widget("ui.message", {
+        options: {
+            attachment: null
+        },
+        editing: false,
+
+        _create: function() {
+            var self = this;
+
+            this.element.bind('click', function(event) {
+                return self.click(event);
+            });
+
+            this.element.addClass("asf-message");
+
+            if (this.options.attachment) {
+                this.attachment(this.options.attachment);
+            }
+        },
+
+        /**
+         * Set the attachment node.
+         *
+         * The message popup is moved near the attachment node.
+         * Display the message if it is hidden.
+         */
+        attachment: function(node) {
+            this.options.attachment = $(node);
+
+            this.element.show('fast');
+            if (this.options.attachment) {
+                var offset = $(this.options.attachment).offset();
+                this.element.offset(offset);
+            }
+        },
+
+        /**
+         * Click action called when the user clicks on the message.
+         * Hide the message.
+         *
+         * @param event the click event
+         */
+        click: function(event) {
+            this.element.hide('fast');
+        }
+    });
+
+})( jQuery );

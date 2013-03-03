6491a593b689f995e714a57911752bd98f7bb91e
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-03-03 19:10:42 +0000

    Change the Name parameter to a Binding access

--- src/asf-views-nodes-jsf.adb
+++ src/asf-views-nodes-jsf.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  views.nodes.jsf -- JSF Core Tag Library
---  Copyright (C) 2010, 2011, 2012 Stephane Carrez
+--  Copyright (C) 2010, 2011, 2012, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,10 +33,10 @@ package body ASF.Views.Nodes.Jsf is
    --  ------------------------------
    --  Create the Converter Tag
    --  ------------------------------
-   function Create_Converter_Tag_Node (Name       : Unbounded_String;
-                                       Line       : Views.Line_Info;
-                                       Parent     : Views.Nodes.Tag_Node_Access;
-                                       Attributes : Views.Nodes.Tag_Attribute_Array_Access)
+   function Create_Converter_Tag_Node (Binding    : in Binding_Access;
+                                       Line       : in Views.Line_Info;
+                                       Parent     : in Views.Nodes.Tag_Node_Access;
+                                       Attributes : in Views.Nodes.Tag_Attribute_Array_Access)
                                        return Views.Nodes.Tag_Node_Access is
 
       use ASF.Views.Nodes;
@@ -45,7 +45,7 @@ package body ASF.Views.Nodes.Jsf is
       Conv : constant Tag_Attribute_Access := Find_Attribute (Attributes,
                                                               "converterId");
    begin
-      Initialize (Node.all'Access, Name, Line, Parent, Attributes);
+      Initialize (Node.all'Access, Binding, Line, Parent, Attributes);
       if Conv = null then
          Node.Error ("Missing 'converterId' attribute");
       else
@@ -94,10 +94,10 @@ package body ASF.Views.Nodes.Jsf is
    --  ------------------------------
    --  Create the Validator Tag
    --  ------------------------------
-   function Create_Validator_Tag_Node (Name       : Unbounded_String;
-                                       Line       : Views.Line_Info;
-                                       Parent     : Views.Nodes.Tag_Node_Access;
-                                       Attributes : Views.Nodes.Tag_Attribute_Array_Access)
+   function Create_Validator_Tag_Node (Binding    : in Binding_Access;
+                                       Line       : in Views.Line_Info;
+                                       Parent     : in Views.Nodes.Tag_Node_Access;
+                                       Attributes : in Views.Nodes.Tag_Attribute_Array_Access)
                                        return Views.Nodes.Tag_Node_Access is
 
       use ASF.Views.Nodes;
@@ -106,7 +106,7 @@ package body ASF.Views.Nodes.Jsf is
       Vid  : constant Tag_Attribute_Access := Find_Attribute (Attributes,
                                                               "validatorId");
    begin
-      Initialize (Node.all'Access, Name, Line, Parent, Attributes);
+      Initialize (Node.all'Access, Binding, Line, Parent, Attributes);
       if Vid = null then
          Node.Error ("Missing 'validatorId' attribute");
       else
@@ -167,17 +167,17 @@ package body ASF.Views.Nodes.Jsf is
    --  ------------------------------
 
    --  Create the Range_Validator Tag
-   function Create_Range_Validator_Tag_Node (Name       : Unbounded_String;
-                                             Line       : Views.Line_Info;
-                                             Parent     : Views.Nodes.Tag_Node_Access;
-                                             Attributes : Views.Nodes.Tag_Attribute_Array_Access)
+   function Create_Range_Validator_Tag_Node (Binding    : in Binding_Access;
+                                             Line       : in Views.Line_Info;
+                                             Parent     : in Views.Nodes.Tag_Node_Access;
+                                             Attributes : in Views.Nodes.Tag_Attribute_Array_Access)
                                              return Views.Nodes.Tag_Node_Access is
 
       use ASF.Views.Nodes;
 
       Node : constant Range_Validator_Tag_Node_Access := new Range_Validator_Tag_Node;
    begin
-      Initialize (Node.all'Access, Name, Line, Parent, Attributes);
+      Initialize (Node.all'Access, Binding, Line, Parent, Attributes);
       Node.Minimum := Find_Attribute (Attributes, "minimum");
       Node.Maximum := Find_Attribute (Attributes, "maximum");
       if Node.Minimum = null and Node.Maximum = null then
@@ -231,17 +231,17 @@ package body ASF.Views.Nodes.Jsf is
    --  Create the Length_Validator Tag.  Verifies that the XML node defines
    --  the <b>minimum</b> or the <b>maximum</b> or both attributes.
    --  ------------------------------
-   function Create_Length_Validator_Tag_Node (Name       : Unbounded_String;
-                                              Line       : Views.Line_Info;
-                                              Parent     : Views.Nodes.Tag_Node_Access;
-                                              Attributes : Views.Nodes.Tag_Attribute_Array_Access)
+   function Create_Length_Validator_Tag_Node (Binding    : in Binding_Access;
+                                              Line       : in Views.Line_Info;
+                                              Parent     : in Views.Nodes.Tag_Node_Access;
+                                              Attributes : in Views.Nodes.Tag_Attribute_Array_Access)
                                               return Views.Nodes.Tag_Node_Access is
 
       use ASF.Views.Nodes;
 
       Node : constant Length_Validator_Tag_Node_Access := new Length_Validator_Tag_Node;
    begin
-      Initialize (Node.all'Access, Name, Line, Parent, Attributes);
+      Initialize (Node.all'Access, Binding, Line, Parent, Attributes);
       Node.Minimum := Find_Attribute (Attributes, "minimum");
       Node.Maximum := Find_Attribute (Attributes, "maximum");
       if Node.Minimum = null and Node.Maximum = null then
@@ -296,21 +296,25 @@ package body ASF.Views.Nodes.Jsf is
    --  ------------------------------
    --  Create the Attribute Tag
    --  ------------------------------
-   function Create_Attribute_Tag_Node (Name       : Unbounded_String;
-                                       Line       : Views.Line_Info;
-                                       Parent     : Views.Nodes.Tag_Node_Access;
-                                       Attributes : Views.Nodes.Tag_Attribute_Array_Access)
+   function Create_Attribute_Tag_Node (Binding    : in Binding_Access;
+                                       Line       : in Views.Line_Info;
+                                       Parent     : in Views.Nodes.Tag_Node_Access;
+                                       Attributes : in Views.Nodes.Tag_Attribute_Array_Access)
                                        return Views.Nodes.Tag_Node_Access is
 
       use ASF.Views.Nodes;
 
-      Node : constant Attribute_Tag_Node_Access := new Attribute_Tag_Node;
+      Attr : constant Tag_Attribute_Access := Find_Attribute (Attributes, "name");
+      Node : Attribute_Tag_Node_Access;
    begin
-      Initialize (Node.all'Access, Name, Line, Parent, Attributes);
-      Node.Attr_Name := Find_Attribute (Attributes, "name");
+      Node := new Attribute_Tag_Node;
+      Initialize (Node.all'Access, Binding, Line, Parent, Attributes);
+      Node.Attr_Name := Attr;
       Node.Value     := Find_Attribute (Attributes, "value");
       if Node.Attr_Name = null then
          Node.Error ("Missing 'name' attribute");
+      else
+         Node.Attr.Name := Attr.Value;
       end if;
       if Node.Value = null then
          Node.Error ("Missing 'value' attribute");
@@ -332,22 +336,17 @@ package body ASF.Views.Nodes.Jsf is
       use EL.Expressions;
    begin
       if Node.Attr_Name /= null and Node.Value /= null then
-         declare
-            Name  : constant EL.Objects.Object := Get_Value (Node.Attr_Name.all, Context);
-         begin
-            Node.Attr.Name := EL.Objects.To_Unbounded_String (Name);
-            if Node.Value.Binding /= null then
-               declare
-                  Expr : constant EL.Expressions.Expression
-                    := ASF.Views.Nodes.Reduce_Expression (Node.Value.all, Context);
-               begin
-                  Parent.Set_Attribute (Def => Node.Attr'Access, Value => Expr);
-               end;
-            else
-               Parent.Set_Attribute (Def   => Node.Attr'Access,
-                                     Value => Get_Value (Node.Value.all, Context));
-            end if;
-         end;
+         if Node.Value.Binding /= null then
+            declare
+               Expr : constant EL.Expressions.Expression
+                 := ASF.Views.Nodes.Reduce_Expression (Node.Value.all, Context);
+            begin
+               Parent.Set_Attribute (Def => Node.Attr'Access, Value => Expr);
+            end;
+         else
+            Parent.Set_Attribute (Def   => Node.Attr'Access,
+                                  Value => Get_Value (Node.Value.all, Context));
+         end if;
       end if;
    end Build_Components;
 
@@ -358,16 +357,16 @@ package body ASF.Views.Nodes.Jsf is
    --  ------------------------------
    --  Create the Facet Tag
    --  ------------------------------
-   function Create_Facet_Tag_Node (Name       : Unbounded_String;
-                                   Line       : Views.Line_Info;
-                                   Parent     : Views.Nodes.Tag_Node_Access;
-                                   Attributes : Views.Nodes.Tag_Attribute_Array_Access)
+   function Create_Facet_Tag_Node (Binding    : in Binding_Access;
+                                   Line       : in Views.Line_Info;
+                                   Parent     : in Views.Nodes.Tag_Node_Access;
+                                   Attributes : in Views.Nodes.Tag_Attribute_Array_Access)
                                    return Views.Nodes.Tag_Node_Access is
       use ASF.Views.Nodes;
 
       Node : constant Facet_Tag_Node_Access := new Facet_Tag_Node;
    begin
-      Initialize (Node.all'Access, Name, Line, Parent, Attributes);
+      Initialize (Node.all'Access, Binding, Line, Parent, Attributes);
       Node.Facet_Name := Find_Attribute (Attributes, "name");
       if Node.Facet_Name = null then
          Node.Error ("Missing 'name' attribute");
@@ -396,16 +395,16 @@ package body ASF.Views.Nodes.Jsf is
    --  ------------------------------
    --  Create the Metadata Tag
    --  ------------------------------
-   function Create_Metadata_Tag_Node (Name       : Unbounded_String;
-                                      Line       : Views.Line_Info;
-                                      Parent     : Views.Nodes.Tag_Node_Access;
-                                      Attributes : Views.Nodes.Tag_Attribute_Array_Access)
+   function Create_Metadata_Tag_Node (Binding    : in Binding_Access;
+                                      Line       : in Views.Line_Info;
+                                      Parent     : in Views.Nodes.Tag_Node_Access;
+                                      Attributes : in Views.Nodes.Tag_Attribute_Array_Access)
                                       return Views.Nodes.Tag_Node_Access is
       use ASF.Views.Nodes;
 
       Node : constant Metadata_Tag_Node_Access := new Metadata_Tag_Node;
    begin
-      Initialize (Node.all'Access, Name, Line, Parent, Attributes);
+      Initialize (Node.all'Access, Binding, Line, Parent, Attributes);
       return Node.all'Access;
    end Create_Metadata_Tag_Node;
 

71245dedcb7e7910e3a92cfc3efaa8135dbf4d2d
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-06-29 21:02:11 +0000

    New UI component ui.editfield to implement editable fields

--- web/js/asf-editable.js
+++ web/js/asf-editable.js
@@ -18,6 +18,90 @@
 (function($, undefined) {
 
     /**
+     * The editfield component switches the content of a <div> element to an <input> field
+     * that can be edited.  The <tt>enterEdit</tt> operation switch the <div> element to
+     * the <input> field and sets the ".asf-editing" CSS class on the element.
+     * When <tt>finishEdit</tt> operation is called, the input value is compared to
+     * the original and the <tt>onChange</tt> operation is called.  The ".asf-editing" CSS
+     * class is removed and the element is restored (ie, the input field is removed).
+     */
+    $.widget("ui.editfield", {
+        options: {
+            /**
+             * Operation called when the component has finished to be edited and the value changed.
+             */
+            onChange: null,
+        },
+        originalValue: null,
+        inputField: null,
+        keyPressHandler: null,
+        clickHandler: null,
+
+        _create: function() {
+            var self = this;
+
+            this.clickHandler = function(event) {
+                if (!$(event.target).closest('.asf-editing').length) {
+                    self.finishEdit();
+                }
+            };
+            this.keyPressHandler = function(event) {
+                if (event.keyCode == 27) {
+                    self.finishEdit();
+                }
+            };
+        },
+
+	    _setOption: function(key, value){
+		    var self = this;
+
+		    switch (key) {
+			case "onChange":
+			    self.options.onChange = value;
+			    break;
+			}
+			$.Widget.prototype._setOption.apply(self, arguments);
+	    },
+
+        createInput: function() {
+            this.inputField = document.createElement("input");
+        },
+
+        /**
+         * Enter the edit mode for the item.
+         *
+         */
+        enterEdit: function() {
+            this.element.originalValue = this.element.text();
+            this.element.addClass("asf-editing");
+            if (this.inputField == null) {
+                this.createInput();
+            }
+            this.inputField.value = this.element.originalValue;
+            this.element.empty();
+            this.element.append(this.inputField);
+
+            /**
+             * Catch Escape and click outside of field to leave the edit mode.
+             */
+            $(document).bind('keyup', this.keyPressHandler).bind('click', this.clickHandler);
+        },
+
+        /**
+         * Finish the edit mode.
+         */
+        finishEdit: function() {
+            $(this.inputField).detach();
+            this.element.text(this.inputField.value);
+            this.element.removeClass("asf-editing");
+            $(document).unbind('keyup', this.keyPressHandler).unbind('click', this.clickHandler);
+            if (this.inputField.value != this.originalValue && this.options.onChange != null) {
+                this.options.onChange(this.element, this.inputField.value);
+            }
+        }
+    });
+
+    /**
      * The editable component enters in edit mode when the user clicks on it.
      * The editing mode is activated by the 'asf-editing' CSS which puts the component in editing mode
      * and makes the input fields visible.

ce2120055cffc68083998bdc4e812e17663acea0
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-08-17 18:40:12 +0000

    Use the List_Holder interface for the management of scrolling lists

--- src/asf-components-utils-scrollers.adb
+++ src/asf-components-utils-scrollers.adb
@@ -21,15 +21,89 @@ with ASF.Components.Html;
 package body ASF.Components.Utils.Scrollers is
 
    --  ------------------------------
-   --  UIScroller
+   --  Get the list value holder that the scroller is controlling.
    --  ------------------------------
+   function Get_List (UI      : in UIScroller;
+                      Context : in ASF.Contexts.Faces.Faces_Context'Class)
+                      return Holders.List_Holder_Access is
+      use type Base.UIComponent_Access;
+
+      Id : constant String := UI.Get_Attribute ("for", Context, "");
+      C  : Base.UIComponent_Access;
+   begin
+      C := UI.Find (Id);
+      if C = null then
+         return null;
+      elsif not (C.all in Holders.List_Holder'Class) then
+         return null;
+      else
+         return Holders.List_Holder'Class (C.all)'Access;
+      end if;
+   end Get_List;
 
    --  Encode the data scroller.
    overriding
    procedure Encode_Children (UI      : in UIScroller;
                               Context : in out ASF.Contexts.Faces.Faces_Context'Class) is
+      use type Holders.List_Holder_Access;
+
+      List : Holders.List_Holder_Access;
    begin
-      null;
+      if not UI.Is_Rendered (Context) then
+         return;
+      end if;
+      List := UIScroller'Class (UI).Get_List (Context);
+      if List = null then
+         Base.Log_Error (UI, "There is no list object associated with the scroller");
+         return;
+      end if;
+      declare
+         Id     : constant Ada.Strings.Unbounded.Unbounded_String := UI.Get_Client_Id;
+         Writer : constant Contexts.Writer.Response_Writer_Access := Context.Get_Response_Writer;
+         Row_Count    : constant Natural := List.Get_Row_Count; --  (Context);
+         Row_Per_Page : constant Natural := List.Get_Row_Per_Page; --  (Context);
+         Current_Page : constant Natural := List.Get_Current_Page; --  (Context);
+         Page_Count   : constant Natural := (Row_Count + Row_Per_Page - 1) / Row_Per_Page;
+      begin
+         Writer.Start_Element ("div");
+         UI.Render_Attributes (Context, Writer);
+
+         if Current_Page > 1 then
+            UI.Render_Page ("first", 1, Context);
+         end if;
+         if Current_Page > 1 then
+            UI.Render_Page ("previous", Current_Page - 1, Context);
+         end if;
+
+         if Current_Page < Page_Count then
+            UI.Render_Page ("next", Current_Page + 1, Context);
+         end if;
+         if Current_Page < Page_Count then
+            UI.Render_Page ("last", Page_Count, Context);
+         end if;
+         Writer.End_Element ("div");
+
+         Writer.Queue_Script ("$('#" & Id & "').scroller();");
+      end;
    end Encode_Children;
+
+   procedure Render_Page (UI      : in UIScroller;
+                          Name    : in String;
+                          Page    : in Positive;
+                          Context : in out ASF.Contexts.Faces.Faces_Context'Class) is
+      use type ASF.Components.Base.UIComponent_Access;
+
+      Writer : constant Contexts.Writer.Response_Writer_Access := Context.Get_Response_Writer;
+      F : constant ASF.Components.Base.UIComponent_Access := UI.Get_Facet (Name);
+   begin
+      Writer.Start_Element ("div");
+      Writer.Write_Attribute ("class", "asf-scroll-" & Name);
+      if F /= null then
+         F.Encode_All (Context);
+      else
+         Writer.Write_Text (Positive'Image (Page));
+      end if;
+      Writer.End_Element ("div");
+   end Render_Page;
 
 end ASF.Components.Utils.Scrollers;
--- src/asf-components-utils-scrollers.ads
+++ src/asf-components-utils-scrollers.ads
@@ -18,6 +18,7 @@
 with ASF.Contexts.Faces;
 with ASF.Contexts.Writer;
 with ASF.Components.Html;
+with ASF.Components.Holders;
 package ASF.Components.Utils.Scrollers is
 
    --  ------------------------------
@@ -25,6 +26,16 @@ package ASF.Components.Utils.Scrollers is
    --  ------------------------------
    --
    type UIScroller is new ASF.Components.Html.UIHtmlComponent with private;
+
+   --  Get the list value holder that the scroller is controlling.
+   function Get_List (UI      : in UIScroller;
+                      Context : in ASF.Contexts.Faces.Faces_Context'Class)
+                      return Holders.List_Holder_Access;
+
+   procedure Render_Page (UI      : in UIScroller;
+                          Name    : in String;
+                          Page    : in Positive;
+                          Context : in out ASF.Contexts.Faces.Faces_Context'Class);
 
    --  Encode the data scroller.
    overriding

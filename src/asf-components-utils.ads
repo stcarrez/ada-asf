c19ba494a615a0144291683481f61835c0518824
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-08-25 21:49:14 +0000

    New function Get_List_Bean to get a bean instance implementing the List_Bean interface

--- src/asf-components-utils.ads
+++ src/asf-components-utils.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  components-util -- ASF Util Components
---  Copyright (C) 2009, 2010, 2011 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +16,10 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
+with Util.Beans.Basic;
+
 with ASF.Components.Base;
+with ASF.Contexts.Faces;
 package ASF.Components.Utils is
 
    --  Get the line information where the component is defined.
@@ -24,5 +27,12 @@ package ASF.Components.Utils is
 
    --  Get the line information where the component is defined.
    function Get_Line_Info (UI : in Base.UIComponent'Class) return String;
+
+   --  Get the component attribute that implements the <tt>List_Bean</tt> interface.
+   --  Returns null if the attribute is not found or does not implement the interface.
+   function Get_List_Bean (UI      : in Base.UIComponent'Class;
+                           Name    : in String;
+                           Context : in ASF.Contexts.Faces.Faces_Context'Class)
+                           return Util.Beans.Basic.List_Bean_Access;
 
 end ASF.Components.Utils;

3703ca979ac0429b8536aeb636412ec99139acd3
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-10-05 14:14:52 +0000

    New function to reformat the XML

--- samples/web/js/samples.js
+++ samples/web/js/samples.js
@@ -22,3 +22,63 @@ function Sample_Highlight(name, keywords) {
       }), [name]);
     prettyPrint();
 }
+
+/**
+ * @brief Split long lines.
+ *
+ * @param text the text to split.
+ * @return the text with long lines split.
+ */
+function splitLongLines(text) {
+    var lines = text.split('\n');
+    var i, j, txt = '';
+
+    for (i = 0; i < lines.length; i++) {
+        if (lines[i].length < 120) {
+            txt = txt + '\n' + lines[i];
+        } else {
+            var l = lines[i].match(/.{1,120}/g);
+            for (j = 0; j < l.length; j++) {
+                txt = txt + '\n' + l[j];
+            }
+        }
+    }
+    return txt;
+}
+
+/**
+ * @brief Reformat the XML.
+ *
+ * @param xml the XML to reformat.
+ * @return the re-indended XML.
+ */
+function formatXml(xml) {
+    var formatted = '';
+    var reg = /(>)(<)(\/*)/g;
+    xml = xml.replace(reg, '$1\r\n$2$3');
+    var pad = 0;
+    jQuery.each(xml.split('\r\n'), function(index, node) {
+        var indent = 0;
+        if (node.match( /.+<\/\w[^>]*>$/ )) {
+            indent = 0;
+        } else if (node.match( /^<\/\w/ )) {
+            if (pad != 0) {
+                pad -= 1;
+            }
+        } else if (node.match( /^<\w[^>]*[^\/]>.*$/ )) {
+            indent = 1;
+        } else {
+            indent = 0;
+        }
+
+        var padding = '';
+        for (var i = 0; i < pad; i++) {
+            padding += '  ';
+        }
+
+        formatted += padding + node + '\r\n';
+        pad += indent;
+    });
+
+    return formatted;
+}

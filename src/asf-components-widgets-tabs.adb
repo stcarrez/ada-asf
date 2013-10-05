2d39e9948914fbaff57f498bff149417e8fa8ef3
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-10-05 21:15:07 +0000

    Implement the new <w:tab> and <w:tabView> components

---
+++ src/asf-components-widgets-tabs.adb
@@ -0,0 +1,108 @@
+-----------------------------------------------------------------------
+--  components-widgets-tabs -- Tab views and tabs
+--  Copyright (C) 2013 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Components.Base;
+with ASF.Contexts.Writer;
+package body ASF.Components.Widgets.Tabs is
+
+   --  ------------------------------
+   --  Render the tab start.
+   --  ------------------------------
+   overriding
+   procedure Encode_Begin (UI      : in UITab;
+                           Context : in out ASF.Contexts.Faces.Faces_Context'Class) is
+      Writer  : constant Contexts.Writer.Response_Writer_Access := Context.Get_Response_Writer;
+   begin
+      if UI.Is_Rendered (Context) then
+         Writer.Start_Element ("div");
+         Writer.Write_Attribute ("id", UI.Get_Client_Id);
+      end if;
+   end Encode_Begin;
+
+   --  ------------------------------
+   --  Render the tab close.
+   --  ------------------------------
+   overriding
+   procedure Encode_End (UI      : in UITab;
+                         Context : in out ASF.Contexts.Faces.Faces_Context'Class) is
+      Writer  : constant Contexts.Writer.Response_Writer_Access := Context.Get_Response_Writer;
+   begin
+      if UI.Is_Rendered (Context) then
+         Writer.End_Element ("div");
+      end if;
+   end Encode_End;
+
+   --  ------------------------------
+   --  Render the tab list and prepare to render the tab contents.
+   --  ------------------------------
+   overriding
+   procedure Encode_Begin (UI      : in UITabView;
+                           Context : in out ASF.Contexts.Faces.Faces_Context'Class) is
+
+      procedure Render_Tab (T : in Components.Base.UIComponent_Access);
+
+      Writer  : constant Contexts.Writer.Response_Writer_Access := Context.Get_Response_Writer;
+
+      procedure Render_Tab (T : in Components.Base.UIComponent_Access) is
+         Id : constant Unbounded_String := T.Get_Client_Id;
+      begin
+         if T.all in UITab'Class then
+            Writer.Start_Element ("li");
+            Writer.Start_Element ("a");
+            Writer.Write_Attribute ("href", "#" & To_String (Id));
+            Writer.Write_Text (T.Get_Attribute ("title", Context));
+            Writer.End_Element ("a");
+            Writer.End_Element ("li");
+         end if;
+      end Render_Tab;
+
+      procedure Render_Tabs is
+        new ASF.Components.Base.Iterate (Process => Render_Tab);
+
+   begin
+      if UI.Is_Rendered (Context) then
+         declare
+            Id : constant Unbounded_String := UI.Get_Client_Id;
+         begin
+            Writer.Start_Element ("div");
+            Writer.Write_Attribute ("id", Id);
+            Writer.Start_Element ("ul");
+            Render_Tabs (UI);
+            Writer.End_Element ("ul");
+            Writer.Queue_Script ("$(""#");
+            Writer.Queue_Script (Id);
+            Writer.Queue_Script (""").tabs({");
+            Writer.Queue_Script ("});");
+         end;
+      end if;
+   end Encode_Begin;
+
+   --  ------------------------------
+   --  Render the tab view close.
+   --  ------------------------------
+   overriding
+   procedure Encode_End (UI      : in UITabView;
+                         Context : in out ASF.Contexts.Faces.Faces_Context'Class) is
+      Writer  : constant Contexts.Writer.Response_Writer_Access := Context.Get_Response_Writer;
+   begin
+      if UI.Is_Rendered (Context) then
+         Writer.End_Element ("div");
+      end if;
+   end Encode_End;
+
+end ASF.Components.Widgets.Tabs;

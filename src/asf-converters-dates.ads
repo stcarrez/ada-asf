4da4f785cf046c502e4959c7b5ea9c27307bb3e5
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-03-08 16:35:46 +0000

    Add support for various date conversion formats defined in JSR-314 - new type Style_Type to define the pre-defined date and time formats - new type Format_Type to define the global date+time or pattern format - new operation Get_Locale to return the date conversion target locale - new function Create_Date_Converter to build the date converter instance - define in the Date_Converter instance the date, time, locale and pattern

--- src/asf-converters-dates.ads
+++ src/asf-converters-dates.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  asf-converters-dates -- Date Converters
---  Copyright (C) 2011 Stephane Carrez
+--  Copyright (C) 2011, 2014 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,9 +15,12 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
 with Util.Beans.Objects;
+with Util.Locales;
 with ASF.Components.Base;
 with ASF.Contexts.Faces;
+with ASF.Locales;
 
 --  The <b>ASF.Converters.Dates</b> defines the date converter to format a date object
 --  into a localized representation.
@@ -26,6 +29,10 @@ with ASF.Contexts.Faces;
 --  (To_String is the JSF getAsString method and To_Object is the JSF getAsObject method)
 package ASF.Converters.Dates is
 
+   type Style_Type is (DEFAULT, SHORT, MEDIUM, LONG, FULL);
+
+   type Format_Type is (DATE, TIME, BOTH, CONVERTER_PATTERN, COMPONENT_FORMAT);
+
    --  ------------------------------
    --  Converter
    --  ------------------------------
@@ -33,7 +40,7 @@ package ASF.Converters.Dates is
    --  into a printable date representation.  It translates a string into an Ada Calendar time.
    --  Unlike the Java implementation, the instance will be shared by multiple
    --  views and requests.
-   type Date_Converter is new Converter with null record;
+   type Date_Converter is new Converter with private;
    type Date_Converter_Access is access all Date_Converter'Class;
 
    --  Convert the object value into a string.  The object value is associated
@@ -54,6 +61,29 @@ package ASF.Converters.Dates is
    --  Get the date format pattern that must be used for formatting a date on the given component.
    function Get_Pattern (Convert   : in Date_Converter;
                          Context   : in ASF.Contexts.Faces.Faces_Context'Class;
+                         Bundle    : in ASF.Locales.Bundle;
                          Component : in ASF.Components.Base.UIComponent'Class) return String;
+
+   --  Get the locale that must be used to format the date.
+   function Get_Locale (Convert   : in Date_Converter;
+                        Context   : in ASF.Contexts.Faces.Faces_Context'Class)
+                        return Util.Locales.Locale;
+
+   --  Create a date converter.
+   function Create_Date_Converter (Date    : in Style_Type;
+                                   Time    : in Style_Type;
+                                   Format  : in Format_Type;
+                                   Locale  : in String;
+                                   Pattern : in String) return Date_Converter_Access;
+
+private
+
+   type Date_Converter is new Converter with record
+      Date_Style : Style_Type := DEFAULT;
+      Time_Style : Style_Type := DEFAULT;
+      Format     : Format_Type;
+      Locale     : Util.Locales.Locale;
+      Pattern    : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
 
 end ASF.Converters.Dates;

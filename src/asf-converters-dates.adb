c02dae18be6723c670d840c49b2469c93baacf14
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-03-08 16:37:51 +0000

    - New function Get_Date_Format and Get_Time_Format to get the pattern   associated with a pre-defined date/time format - Update the Get_Pattern to retrieve either a pre-defined pattern,   a custom pattern in the converter or a custom pattern in the component - Implement the new function Get_Locale - Implement the new function Create_Date_Converter - Use the above for the date conversion

--- src/asf-converters-dates.adb
+++ src/asf-converters-dates.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  asf-converters-dates -- Date Converters
---  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2014 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,28 +25,109 @@ with Util.Dates.Formats;
 with Util.Log.Loggers;
 
 with ASF.Applications.Main;
-with ASF.Locales;
 package body ASF.Converters.Dates is
-
-   use Util.Log;
 
    --  The logger
    Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("ASF.Converters.Dates");
+
+   function Get_Date_Format (Mode   : in Style_Type;
+                             Bundle : in ASF.Locales.Bundle) return String;
+   function Get_Time_Format (Mode   : in Style_Type;
+                             Bundle : in ASF.Locales.Bundle) return String;
+
+   --  ------------------------------
+   --  Get the format to print the date.
+   --  ------------------------------
+   function Get_Date_Format (Mode   : in Style_Type;
+                             Bundle : in ASF.Locales.Bundle) return String is
+   begin
+      case Mode is
+         when DEFAULT =>
+            return Bundle.Get ("faces.dates.default_date_format", "%x");
+
+         when SHORT =>
+            return Bundle.Get ("faces.dates.short_date_format", "%d/%m/%Y");
+
+         when MEDIUM =>
+            return Bundle.Get ("faces.dates.medium_date_format", "%b %d, %Y");
+
+         when LONG =>
+            return Bundle.Get ("faces.dates.long_date_format", "%B %d, %Y");
+
+         when FULL =>
+            return Bundle.Get ("faces.dates.full_date_format", "%A, %B %d, %Y");
+
+      end case;
+   end Get_Date_Format;
+
+   --  ------------------------------
+   --  Get the format to print the time.
+   --  ------------------------------
+   function Get_Time_Format (Mode   : in Style_Type;
+                             Bundle : in ASF.Locales.Bundle) return String is
+   begin
+      case Mode is
+         when DEFAULT =>
+            return Bundle.Get ("faces.dates.default_time_format", "%X");
+
+         when SHORT =>
+            return Bundle.Get ("faces.dates.short_time_format", "%H:%M");
+
+         when MEDIUM =>
+            return Bundle.Get ("faces.dates.medium_time_format", "%H:%M");
+
+         when LONG =>
+            return Bundle.Get ("faces.dates.long_time_format", "%H:%M:%S");
+
+         when FULL =>
+            return Bundle.Get ("faces.dates.full_time_format", "%H:%M:%S %Z");
+
+      end case;
+   end Get_Time_Format;
 
    --  ------------------------------
    --  Get the date format pattern that must be used for formatting a date on the given component.
    --  ------------------------------
    function Get_Pattern (Convert   : in Date_Converter;
                          Context   : in ASF.Contexts.Faces.Faces_Context'Class;
+                         Bundle    : in ASF.Locales.Bundle;
                          Component : in ASF.Components.Base.UIComponent'Class) return String is
-      pragma Unreferenced (Convert);
-
-      Pattern : constant String := Component.Get_Attribute (Context => Context,
-                                                            Name    => "format",
-                                                            Default => "%x");
    begin
-      return Pattern;
+      case Convert.Format is
+         when BOTH =>
+            return Get_Date_Format (Convert.Time_Style, Bundle);
+
+         when TIME =>
+            return Get_Time_Format (Convert.Time_Style, Bundle);
+
+         when DATE =>
+            return Get_Date_Format (Convert.Date_Style, Bundle);
+
+         when CONVERTER_PATTERN =>
+            return Ada.Strings.Unbounded.To_String (Convert.Pattern);
+
+         when COMPONENT_FORMAT =>
+            return Component.Get_Attribute (Context => Context,
+                                            Name    => "format",
+                                            Default => "%x");
+
+      end case;
    end Get_Pattern;
+
+   --  ------------------------------
+   --  Get the locale that must be used to format the date.
+   --  ------------------------------
+   function Get_Locale (Convert   : in Date_Converter;
+                        Context   : in ASF.Contexts.Faces.Faces_Context'Class)
+                        return Util.Locales.Locale is
+      use type Util.Locales.Locale;
+   begin
+      if Convert.Locale /= Util.Locales.NULL_LOCALE then
+         return Convert.Locale;
+      else
+         return Context.Get_Locale;
+      end if;
+   end Get_Locale;
 
    --  ------------------------------
    --  Convert the object value into a string.  The object value is associated
@@ -57,13 +138,14 @@ package body ASF.Converters.Dates is
                        Context   : in ASF.Contexts.Faces.Faces_Context'Class;
                        Component : in ASF.Components.Base.UIComponent'Class;
                        Value     : in Util.Beans.Objects.Object) return String is
+      Locale  : constant Util.Locales.Locale
+        := Date_Converter'Class (Convert).Get_Locale (Context);
       Bundle  : ASF.Locales.Bundle;
-      Pattern : constant String := Date_Converter'Class (Convert).Get_Pattern (Context, Component);
    begin
       begin
          ASF.Applications.Main.Load_Bundle (Context.Get_Application.all,
-                                            Name   => "dates",
-                                            Locale => "en",
+                                            Name   => "asf",
+                                            Locale => Util.Locales.To_String (Locale),
                                             Bundle => Bundle);
 
       exception
@@ -73,6 +155,8 @@ package body ASF.Converters.Dates is
 
       --  Convert the value as a date here so that we can raise an Invalid_Conversion exception.
       declare
+         Pattern : constant String
+           := Date_Converter'Class (Convert).Get_Pattern (Context, Bundle, Component);
          Date    : constant Ada.Calendar.Time := Util.Beans.Objects.Time.To_Time (Value);
          Result  : constant String := Util.Dates.Formats.Format (Pattern, Date, Bundle);
       begin
@@ -96,5 +180,32 @@ package body ASF.Converters.Dates is
       Log.Error ("String to date conversion is not yet implemented");
       return Util.Beans.Objects.Null_Object;
    end To_Object;
+
+   --  ------------------------------
+   --  Create a date converter.
+   --  ------------------------------
+   function Create_Date_Converter (Date    : in Style_Type;
+                                   Time    : in Style_Type;
+                                   Format  : in Format_Type;
+                                   Locale  : in String;
+                                   Pattern : in String) return Date_Converter_Access is
+      use Ada.Strings.Unbounded;
+
+      L : constant Util.Locales.Locale := Util.Locales.Get_Locale (Locale);
+   begin
+      if Pattern'Length > 0 then
+         return new Date_Converter '(Date_Style => Date,
+                                     Time_Style => Time,
+                                     Locale     => L,
+                                     Format     => CONVERTER_PATTERN,
+                                     Pattern    => To_Unbounded_String (Pattern));
+      else
+         return new Date_Converter '(Date_Style => Date,
+                                     Time_Style => Time,
+                                     Format     => Format,
+                                     Locale     => L,
+                                     Pattern    => Null_Unbounded_String);
+      end if;
+   end Create_Date_Converter;
 
 end ASF.Converters.Dates;

e9abc262efeed991ca6ae1322f92058069bd8b0d
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-03-30 13:27:13 +0000

    Register the <h:doctype> component in the factory

--- src/asf-components-html-factory.adb
+++ src/asf-components-html-factory.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  html-factory -- Factory for HTML UI Components
---  Copyright (C) 2009, 2010, 2011, 2012 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2011, 2012, 2014 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,6 +31,7 @@ package body ASF.Components.Html.Factory is
    use ASF.Components.Base;
 
    function Create_Body return UIComponent_Access;
+   function Create_Doctype return UIComponent_Access;
    function Create_Head return UIComponent_Access;
    function Create_Output return UIComponent_Access;
    function Create_Output_Label return UIComponent_Access;
@@ -59,6 +60,14 @@ package body ASF.Components.Html.Factory is
    begin
       return new ASF.Components.Html.Pages.UIBody;
    end Create_Body;
+
+   --  ------------------------------
+   --  Create an UIDoctype component
+   --  ------------------------------
+   function Create_Doctype return UIComponent_Access is
+   begin
+      return new ASF.Components.Html.Pages.UIDoctype;
+   end Create_Doctype;
 
    --  ------------------------------
    --  Create an UIHead component
@@ -220,6 +229,7 @@ package body ASF.Components.Html.Factory is
 
    BODY_TAG           : aliased constant String := "body";
    COMMAND_BUTTON_TAG : aliased constant String := "commandButton";
+   DOCTYPE_TAG        : aliased constant String := "doctype";
    FORM_TAG           : aliased constant String := "form";
    HEAD_TAG           : aliased constant String := "head";
    INPUT_FILE_TAG     : aliased constant String := "inputFile";
@@ -246,58 +256,61 @@ package body ASF.Components.Html.Factory is
          2 => (Name      => COMMAND_BUTTON_TAG'Access,
                Component => Create_Command'Access,
                Tag       => Create_Component_Node'Access),
-         3 => (Name      => FORM_TAG'Access,
+         3 => (Name      => DOCTYPE_TAG'Access,
+               Component => Create_Doctype'Access,
+               Tag       => Create_Component_Node'Access),
+         4 => (Name      => FORM_TAG'Access,
                Component => Create_Form'Access,
                Tag       => Create_Component_Node'Access),
-         4 => (Name      => HEAD_TAG'Access,
+         5 => (Name      => HEAD_TAG'Access,
                Component => Create_Head'Access,
                Tag       => Create_Component_Node'Access),
-         5 => (Name      => INPUT_FILE_TAG'Access,
+         6 => (Name      => INPUT_FILE_TAG'Access,
                Component => Create_Input_File'Access,
                Tag       => Create_Component_Node'Access),
-         6 => (Name      => INPUT_HIDDEN_TAG'Access,
+         7 => (Name      => INPUT_HIDDEN_TAG'Access,
                Component => Create_Input_Hidden'Access,
                Tag       => Create_Component_Node'Access),
-         7 => (Name      => INPUT_SECRET_TAG'Access,
+         8 => (Name      => INPUT_SECRET_TAG'Access,
                Component => Create_Input_Secret'Access,
                Tag       => Create_Component_Node'Access),
-         8 => (Name      => INPUT_TEXT_TAG'Access,
+         9 => (Name      => INPUT_TEXT_TAG'Access,
                Component => Create_Input_Text'Access,
                Tag       => Create_Component_Node'Access),
-         9 => (Name      => INPUT_TEXTAREA_TAG'Access,
+        10 => (Name      => INPUT_TEXTAREA_TAG'Access,
                Component => Create_Input_Textarea'Access,
                Tag       => Create_Component_Node'Access),
-        10 => (Name      => LIST_TAG'Access,
+        11 => (Name      => LIST_TAG'Access,
                Component => Create_List'Access,
                Tag       => Create_Component_Node'Access),
-        11 => (Name      => MESSAGE_TAG'Access,
+        12 => (Name      => MESSAGE_TAG'Access,
                Component => Create_Message'Access,
                Tag       => Create_Component_Node'Access),
-        12 => (Name      => MESSAGES_TAG'Access,
+        13 => (Name      => MESSAGES_TAG'Access,
                Component => Create_Messages'Access,
                Tag       => Create_Component_Node'Access),
-        13 => (Name      => OUTPUT_FORMAT_TAG'Access,
+        14 => (Name      => OUTPUT_FORMAT_TAG'Access,
                Component => Create_Output_Format'Access,
                Tag       => Create_Component_Node'Access),
-        14 => (Name      => OUTPUT_LABEL_TAG'Access,
+        15 => (Name      => OUTPUT_LABEL_TAG'Access,
                Component => Create_Output_Label'Access,
                Tag       => Create_Component_Node'Access),
-        15 => (Name      => OUTPUT_LINK_TAG'Access,
+        16 => (Name      => OUTPUT_LINK_TAG'Access,
                Component => Create_Output_Link'Access,
                Tag       => Create_Component_Node'Access),
-        16 => (Name      => OUTPUT_TEXT_TAG'Access,
+        17 => (Name      => OUTPUT_TEXT_TAG'Access,
                Component => Create_Output'Access,
                Tag       => Create_Component_Node'Access),
-        17 => (Name      => PANEL_GROUP_TAG'Access,
+        18 => (Name      => PANEL_GROUP_TAG'Access,
                Component => Create_PanelGroup'Access,
                Tag       => Create_Component_Node'Access),
-        18 => (Name      => SELECT_BOOLEAN_TAG'Access,
+        19 => (Name      => SELECT_BOOLEAN_TAG'Access,
                Component => Create_SelectBooleanCheckbox'Access,
                Tag       => Create_Component_Node'Access),
-        19 => (Name      => SELECT_ONE_MENU_TAG'Access,
+        20 => (Name      => SELECT_ONE_MENU_TAG'Access,
                Component => Create_SelectOne'Access,
                Tag       => Create_Component_Node'Access),
-        20 => (Name      => SELECT_ONE_RADIO_TAG'Access,
+        21 => (Name      => SELECT_ONE_RADIO_TAG'Access,
                Component => Create_SelectOneRadio'Access,
                Tag       => Create_Component_Node'Access)
         );

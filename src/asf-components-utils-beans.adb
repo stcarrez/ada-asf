b8d7d643abcda9ec7e7edb69831b1cbcb7a45459
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-06-19 20:42:11 +0200

    Fix the <util:set> component to set a variable or a bean attribute

--- src/asf-components-utils-beans.adb
+++ src/asf-components-utils-beans.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  components-utils-beans -- Bean component utility
---  Copyright (C) 2011 Stephane Carrez
+--  Copyright (C) 2011, 2015 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Util.Beans.Objects;
 package body ASF.Components.Utils.Beans is
 
    --  ------------------------------
@@ -25,14 +25,22 @@ package body ASF.Components.Utils.Beans is
    overriding
    procedure Encode_Begin (UI      : in UISetBean;
                            Context : in out ASF.Contexts.Faces.Faces_Context'Class) is
-      VE : constant EL.Expressions.Value_Expression := UI.Get_Value_Expression ("var");
+      Name : constant String := Util.Beans.Objects.To_String (UI.Get_Attribute (Context, "var"));
    begin
-      if VE.Is_Null then
-         UI.Log_Error ("Invalid value expression for 'var'");
-         return;
+      if Name'Length > 0 then
+         Context.Set_Attribute (Name, UI.Get_Attribute (Context => Context, Name => "value"));
+      else
+         declare
+            VE : constant EL.Expressions.Value_Expression := UI.Get_Value_Expression ("var");
+         begin
+            if VE.Is_Null then
+               UI.Log_Error ("Invalid value expression for 'var'");
+               return;
+            end if;
+            VE.Set_Value (Context => Context.Get_ELContext.all,
+                          Value   => UI.Get_Attribute (Context => Context, Name => "value"));
+         end;
       end if;
-      VE.Set_Value (Context => Context.Get_ELContext.all,
-                    Value   => UI.Get_Attribute (Context => Context, Name => "value"));
    end Encode_Begin;
 
 end ASF.Components.Utils.Beans;

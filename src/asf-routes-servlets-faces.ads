97b31a73709cb03aea0420b28ec4507688972fb6
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-09-26 16:58:18 +0200

    Remove unused with clauses

--- src/asf-routes-servlets-faces.ads
+++ src/asf-routes-servlets-faces.ads
@@ -15,19 +15,13 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Ada.Finalization;
+
 with Ada.Strings.Unbounded;
-with ASF.Filters;
-with ASF.Servlets;
 package ASF.Routes.Servlets.Faces is
 
    type Faces_Route_Type is new Servlet_Route_Type with record
       View    : Ada.Strings.Unbounded.Unbounded_String;
    end record;
    type Faces_Route_Type_Access is access all Faces_Route_Type'Class;
-
-   --  Release the storage held by the route.
---     overriding
---     procedure Finalize (Route : in out Servlet_Route_Type);
 
 end ASF.Routes.Servlets.Faces;

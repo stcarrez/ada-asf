881e63179947f9c7fc7ee3b0c96bc11363ded373
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-12-19 13:57:14 +0100

    Declare the Register procedure and make the Entries member a private member

--- src/asf-rest-definition.ads
+++ src/asf-rest-definition.ads
@@ -23,25 +23,39 @@ package ASF.Rest.Definition is
    type Descriptor is new ASF.Rest.Descriptor with record
       Handler  : access procedure (Object : in out Object_Type;
                                    Req    : in out ASF.Rest.Request'Class;
-                                   Reply  : in out ASF.Rest.Response'Class);
+                                   Reply  : in out ASF.Rest.Response'Class;
+                                   Stream : in out ASF.Rest.Output_Stream'Class);
    end record;
 
    overriding
    procedure Dispatch (Handler : in Descriptor;
                        Req     : in out ASF.Rest.Request'Class;
-                       Reply   : in out ASF.Rest.Response'Class);
+                       Reply   : in out ASF.Rest.Response'Class;
+                       Stream  : in out ASF.Rest.Output_Stream'Class);
 
-   Entries : ASF.Rest.Descriptor_Access;
-
+   --  Definition of an API operation mapped to a given URI pattern and associated with
+   --  the operation handler.
    generic
       Handler    : access procedure (Object : in out Object_Type;
                                      Req    : in out ASF.Rest.Request'Class;
-                                     Reply  : in out ASF.Rest.Response'Class);
+                                     Reply  : in out ASF.Rest.Response'Class;
+                                     Stream : in out ASF.Rest.Output_Stream'Class);
       Method     : Method_Type;
       Pattern    : String;
       Permission : Security.Permissions.Permission_Index;
    package Definition is
       Instance : aliased Descriptor;
    end Definition;
+
+   --  Register the list of APIs that have been created by instantiating the <tt>Definition</tt>
+   --  package.  The REST servlet identified by <tt>Name</tt> is searched in the servlet registry
+   --  and used as the servlet for processing the API requests.
+   procedure Register (Registry  : in out ASF.Servlets.Servlet_Registry;
+                       Name      : in String;
+                       ELContext : in EL.Contexts.ELContext'Class);
+
+private
+
+   Entries : ASF.Rest.Descriptor_Access;
 
 end ASF.Rest.Definition;

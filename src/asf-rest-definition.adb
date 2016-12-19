cfefd02fe0c132480466eb6524a44a8498b04f1e
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-12-19 13:57:40 +0100

    Implement the Register procedure to register all the APIs that have been instantiated to the servlet registry

--- src/asf-rest-definition.adb
+++ src/asf-rest-definition.adb
@@ -18,13 +18,30 @@
 
 package body ASF.Rest.Definition is
 
+   --  ------------------------------
+   --  Register the list of APIs that have been created by instantiating the <tt>Definition</tt>
+   --  package.  The REST servlet identified by <tt>Name</tt> is searched in the servlet registry
+   --  and used as the servlet for processing the API requests.
+   --  ------------------------------
+   procedure Register (Registry  : in out ASF.Servlets.Servlet_Registry;
+                       Name      : in String;
+                       ELContext : in EL.Contexts.ELContext'Class) is
+   begin
+      ASF.Rest.Register (Registry => Registry,
+                         Name     => Name,
+                         URI      => URI,
+                         ELContext => ELContext,
+                         List     => Entries);
+   end Register;
+
    overriding
    procedure Dispatch (Handler : in Descriptor;
                        Req     : in out ASF.Rest.Request'Class;
-                       Reply   : in out ASF.Rest.Response'Class) is
+                       Reply   : in out ASF.Rest.Response'Class;
+                       Stream  : in out ASF.Rest.Output_Stream'Class) is
       Object : Object_Type;
    begin
-      Handler.Handler (Object, Req, Reply);
+      Handler.Handler (Object, Req, Reply, Stream);
    end Dispatch;
 
    package body Definition is

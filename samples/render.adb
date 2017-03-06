b4b5ae44894cdd3db063908f5fd40e364a7b8601
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-03-06 20:56:37 +0100

    Fix the render example so that it works with the new ASF routes

--- samples/render.adb
+++ samples/render.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  render -- XHTML Rendering example
---  Copyright (C) 2010 Stephane Carrez
+--  Copyright (C) 2010, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -25,6 +25,9 @@ with GNAT.Command_Line;
 with ASF.Applications.Main;
 with ASF.Requests.Mockup;
 with ASF.Responses.Mockup;
+with ASF.Servlets;
+with ASF.Servlets.Faces;
+with ASF.Server;
 
 with EL.Objects;
 --  This example reads an XHTML file and renders the result.
@@ -37,17 +40,22 @@ procedure Render is
 
    use EL.Objects;
 
-   Factory  : ASF.Applications.Main.Application_Factory;
-   App      : Applications.Main.Application;
-   Conf     : Applications.Config;
+   Factory   : ASF.Applications.Main.Application_Factory;
+   Faces     : aliased ASF.Servlets.Faces.Faces_Servlet;
+   App       : aliased Applications.Main.Application;
+   Conf      : Applications.Config;
+   Container : ASF.Server.Container;
 begin
    Conf.Set ("view.ignore_white_spaces", "false");
    Conf.Set ("view.escape_unknown_tags", "false");
    Conf.Set ("view.ignore_empty_lines", "true");
-   Conf.Set ("view.dir", ".");
+   Conf.Set ("view.dir", "./");
    Conf.Set ("view.file_ext", "");
-
    App.Initialize (Conf, Factory);
+
+   App.Add_Servlet (Name => "file", Server => Faces'Unchecked_Access);
+   App.Add_Mapping ("*.xhtml", "file");
+   Container.Register_Application ("/render", App'Unchecked_Access);
    loop
       case Getopt ("D:") is
          when 'D' =>
@@ -83,10 +91,8 @@ begin
       end if;
 
       Req.Set_Method ("GET");
-      Req.Set_Request_URI (View_Name (View_Name'First .. Pos - 1));
-      App.Dispatch (Page     => View_Name (View_Name'First .. Pos - 1),
-                    Request  => Req,
-                    Response => Reply);
+      Req.Set_Request_URI ("/render/" & View_Name); --  View_Name (View_Name'First .. Pos - 1) & ".html");
+      Container.Service (Req, Reply);
 
       Reply.Read_Content (Content);
       Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (Content));

9d81690b1d2b32a976a5a6e6d799b44883d20f03
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-10 23:54:24 +0200

    Implement the generic function Get_Request_Bean

--- src/asf-helpers-beans.adb
+++ src/asf-helpers-beans.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  asf-helpers-beans -- Helper packages to write ASF applications
---  Copyright (C) 2012 Stephane Carrez
+--  Copyright (C) 2012, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with Util.Beans.Objects;
 with ASF.Contexts.Faces;
 package body ASF.Helpers.Beans is
 
@@ -42,5 +42,22 @@ package body ASF.Helpers.Beans is
          end if;
       end;
    end Get_Bean;
+
+   --  ------------------------------
+   --  Get a bean instance associated under the given name from the request.
+   --  A null value is returned if the bean does not exist or is not of the good type.
+   --  ------------------------------
+   function Get_Request_Bean (Request : in ASF.Requests.Request'Class;
+                              Name    : in String) return Element_Access is
+      Value : constant Util.Beans.Objects.Object := Request.Get_Attribute (Name);
+      Bean  : constant access Util.Beans.Basic.Readonly_Bean'Class
+        := Util.Beans.Objects.To_Bean (Value);
+   begin
+      if Bean = null or else not (Bean.all in Element_Type'Class) then
+         return null;
+      else
+         return Element_Type'Class (Bean.all)'Access;
+      end if;
+   end Get_Request_Bean;
 
 end ASF.Helpers.Beans;

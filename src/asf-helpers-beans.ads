b68c3779eb4d9f2043bb97e60fa34ca4e9a215c5
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-10 23:54:12 +0200

    Declare the Get_Request_Bean generic function

--- src/asf-helpers-beans.ads
+++ src/asf-helpers-beans.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  asf-helpers-beans -- Helper packages to write ASF applications
---  Copyright (C) 2012 Stephane Carrez
+--  Copyright (C) 2012, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,6 +17,7 @@
 -----------------------------------------------------------------------
 with Util.Beans.Basic;
 
+with ASF.Requests;
 package ASF.Helpers.Beans is
 
    --  Get a bean instance associated under the given name from the current faces context.
@@ -25,5 +26,13 @@ package ASF.Helpers.Beans is
       type Element_Type is new Util.Beans.Basic.Readonly_Bean with private;
       type Element_Access is access all Element_Type'Class;
    function Get_Bean (Name : in String) return Element_Access;
+
+   --  Get a bean instance associated under the given name from the request.
+   --  A null value is returned if the bean does not exist or is not of the good type.
+   generic
+      type Element_Type is new Util.Beans.Basic.Readonly_Bean with private;
+      type Element_Access is access all Element_Type'Class;
+   function Get_Request_Bean (Request : in ASF.Requests.Request'Class;
+                              Name    : in String) return Element_Access;
 
 end ASF.Helpers.Beans;

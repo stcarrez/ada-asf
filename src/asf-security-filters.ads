a104b302d859ae43835987b7d864570d7da94d72
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-03-10 11:08:01 +0100

    Package ASF.Security.Filters moved to Servlet.Security.Filters

--- src/asf-security-filters.ads
+++ src/asf-security-filters.ads
@@ -23,6 +23,7 @@ with ASF.Servlets;
 with ASF.Sessions;
 with ASF.Principals;
 with Security.Policies;  use Security;
+with Servlet.Security.Filters;
 
 --  The <b>Security.Filters</b> package defines a servlet filter that can be activated
 --  on requests to authenticate users and verify they have the permission to view
@@ -33,58 +34,6 @@ package ASF.Security.Filters is
 
    AID_COOKIE : constant String := "AID";
 
-   type Auth_Filter is new ASF.Filters.Filter with private;
-
-   --  Called by the servlet container to indicate to a servlet that the servlet
-   --  is being placed into service.
-   overriding
-   procedure Initialize (Server  : in out Auth_Filter;
-                         Config  : in ASF.Servlets.Filter_Config);
-
-   --  Set the permission manager that must be used to verify the permission.
-   procedure Set_Permission_Manager (Filter  : in out Auth_Filter;
-                                     Manager : in Policies.Policy_Manager_Access);
-
-   --  Filter the request to make sure the user is authenticated.
-   --  Invokes the <b>Do_Login</b> procedure if there is no user.
-   --  If a permission manager is defined, check that the user has the permission
-   --  to view the page.  Invokes the <b>Do_Deny</b> procedure if the permission
-   --  is denied.
-   procedure Do_Filter (F        : in Auth_Filter;
-                        Request  : in out ASF.Requests.Request'Class;
-                        Response : in out ASF.Responses.Response'Class;
-                        Chain    : in out ASF.Servlets.Filter_Chain);
-
-   --  Display or redirects the user to the login page.  This procedure is called when
-   --  the user is not authenticated.
-   procedure Do_Login (F        : in Auth_Filter;
-                       Request  : in out ASF.Requests.Request'Class;
-                       Response : in out ASF.Responses.Response'Class);
-
-   --  Display the forbidden access page.  This procedure is called when the user is not
-   --  authorized to see the page.  The default implementation returns the SC_FORBIDDEN error.
-   procedure Do_Deny (F        : in Auth_Filter;
-                      Request  : in out ASF.Requests.Request'Class;
-                      Response : in out ASF.Responses.Response'Class);
-
-   --  Authenticate a user by using the auto-login cookie.  This procedure is called if the
-   --  current session does not have any principal.  Based on the request and the optional
-   --  auto-login cookie passed in <b>Auth_Id</b>, it should identify the user and return
-   --  a principal object.  The principal object will be freed when the session is closed.
-   --  If the user cannot be authenticated, the returned principal should be null.
-   --
-   --  The default implementation returns a null principal.
-   procedure Authenticate (F        : in Auth_Filter;
-                           Request  : in out ASF.Requests.Request'Class;
-                           Response : in out ASF.Responses.Response'Class;
-                           Session  : in ASF.Sessions.Session;
-                           Auth_Id  : in String;
-                           Principal : out ASF.Principals.Principal_Access);
-
-private
-
-   type Auth_Filter is new ASF.Filters.Filter with record
-      Manager : Policies.Policy_Manager_Access := null;
-   end record;
+   subtype Auth_Filter is Servlet.Security.Filters.Auth_Filter;
 
 end ASF.Security.Filters;

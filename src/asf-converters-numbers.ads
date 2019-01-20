2914446476b831a462bbd7c85ebf3a479e0e6786
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-01-20 15:04:36 +0100

    Declare Number_Converter type with operations to convert a floating point number

--- src/asf-converters-numbers.ads
+++ src/asf-converters-numbers.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  asf-converters -- ASF Converters
---  Copyright (C) 2010 Stephane Carrez
+--  asf-converters-numbers -- Floating point number converters
+--  Copyright (C) 2010, 2019 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,45 +15,68 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
---  with EL.Objects;
---  with ASF.Components;
---  with ASF.Contexts.Faces;
+with EL.Objects;
+with ASF.Components;
+with ASF.Contexts.Faces;
+with ASF.Locales;
+private with Util.Locales;
+private with Ada.Text_IO.Editing;
 
---  The <b>ASF.Converters</b> defines an interface used by the conversion model
---  to translate an object into a string when formatting the response and translate
---  a string into an object during the apply request or validation phases (JSF postback).
---
---  See JSR 314 - JavaServer Faces Specification 3.3.2 Converter
---  (To_String is the JSF getAsString method and To_Object is the JSF getAsObject method)
+--  The `ASF.Converters.Numbers` provides a floating point number converter.
+--  It can be used to print floating point numbers in various formats.
 package ASF.Converters.Numbers is
 
-   Invalid_Conversion : exception;
-
-   --  ------------------------------
-   --  Converter
-   --  ------------------------------
-   --  The <b>Object_Converter</b> is a must implement two functions to convert a string into
-   --  an object and the opposite.  The converter instance must be registered in
-   --  the component factory (See <b>ASF.Factory.Component_Factory</b>).
-   --  Unlike the Java implementation, the instance will be shared by multiple
-   --  views and requests.
-   type Object_Converter is abstract new Converter with null record;
+   type Number_Converter is new Converter with private;
+   type Number_Converter_Access is access all Number_Converter'Class;
 
    --  Convert the object value into a string.  The object value is associated
    --  with the specified component.
    --  If the string cannot be converted, the Invalid_Conversion exception should be raised.
---     overriding
---     function To_String (Convert   : in Object_Converter;
---                         Context   : in ASF.Contexts.Faces.Faces_Context'Class;
---                         Component : in ASF.Components.UIComponent'Class;
---                         Value     : in EL.Objects.Object) return String;
+   overriding
+   function To_String (Convert   : in Number_Converter;
+                       Context   : in ASF.Contexts.Faces.Faces_Context'Class;
+                       Component : in ASF.Components.Base.UIComponent'Class;
+                       Value     : in EL.Objects.Object) return String;
 
    --  Convert the string into an object for the specified component.
    --  If the string cannot be converted, the Invalid_Conversion exception should be raised.
---     overriding
---     function To_Object (Convert   : in Object_Converter;
---                         Context   : in ASF.Contexts.Faces.Faces_Context'Class;
---                         Component : in ASF.Components.UIComponent'Class;
---                         Value     : in String) return EL.Objects.Object;
+   overriding
+   function To_Object (Convert   : in Number_Converter;
+                       Context   : in ASF.Contexts.Faces.Faces_Context'Class;
+                       Component : in ASF.Components.Base.UIComponent'Class;
+                       Value     : in String) return EL.Objects.Object;
+
+   --  Set the picture that must be used for the conversion.
+   procedure Set_Picture (Converter : in out Number_Converter;
+                          Picture   : in String);
+
+   --  Get the currency to be used from the resource bundle in the user's current locale.
+   function Get_Currency (Converter : in Number_Converter;
+                          Bundle    : in ASF.Locales.Bundle) return String;
+
+   --  Get the separator to be used from the resource bundle in the user's current locale.
+   function Get_Separator (Converter : in Number_Converter;
+                           Bundle    : in ASF.Locales.Bundle) return Character;
+
+   --  Get the radix mark to be used from the resource bundle in the user's current locale.
+   function Get_Radix_Mark (Converter : in Number_Converter;
+                            Bundle    : in ASF.Locales.Bundle) return Character;
+
+   --  Get the fill character to be used from the resource bundle in the user's current locale.
+   function Get_Fill (Converter : in Number_Converter;
+                      Bundle    : in ASF.Locales.Bundle) return Character;
+
+private
+
+   type Number_Converter is new Converter with record
+      Picture         : Ada.Text_IO.Editing.Picture;
+      Locale          : Util.Locales.Locale;
+      Depend_On_Local : Boolean := False;
+   end record;
+
+   --  Get the locale that must be used to format the number.
+   function Get_Locale (Convert   : in Number_Converter;
+                        Context   : in ASF.Contexts.Faces.Faces_Context'Class)
+                        return Util.Locales.Locale;
 
 end ASF.Converters.Numbers;

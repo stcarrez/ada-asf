f462907d2f1e221da36f8d3b04cdcf8b5fcb5f2f
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-12 23:33:32 +0100

    Update and cleanup coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-lcov --base-directory . --directory . -c -o asf.cov >/dev/null
-bin/asf_harness -t 120 -xml asf-aunit.xml -config test.properties
-(lcov --base-directory . --directory . -c -o asf.cov
-lcov --remove asf.cov "/usr*" -o asf.cov 
-lcov --remove asf.cov "/opt*" -o asf.cov 
-lcov --remove asf.cov "regtests*" -o asf.cov
-lcov --remove asf.cov "*/ada-util/*" -o asf.cov
-lcov --remove asf.cov ada-asf/b__asf_harness.adb -o asf.cov ) >/dev/null
+NAME=asf.cov
+lcov --base-directory . --directory . -c --include "*/ada-asf/src/*" -o $NAME
 rm -rf cover
-genhtml -o ./cover -t "test coverage" --num-spaces 4 asf.cov
+genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  

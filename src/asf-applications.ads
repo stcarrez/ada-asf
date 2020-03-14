2bcfc8584fd2a85668bf8cc6ae11923524ce4f73
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-03-14 17:45:31 +0100

    Add a new Get function that returns the configuration parameter value as integer

--- src/asf-applications.ads
+++ src/asf-applications.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  applications -- Ada Web Application
---  Copyright (C) 2009, 2010, 2013, 2015 Stephane Carrez
+--  Copyright (C) 2009, 2010, 2013, 2015, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -88,6 +88,9 @@ package ASF.Applications is
    --  Get the configuration parameter;
    function Get (Self  : Config;
                  Param : Config_Param) return Boolean;
+
+   function Get (Self  : Config;
+                 Param : Config_Param) return Integer;
 
    --  Create the configuration parameter definition instance.
    generic

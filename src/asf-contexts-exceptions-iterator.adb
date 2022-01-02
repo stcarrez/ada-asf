62081e230c8727a40a61c883a4fe4873e71f98d0
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-01-02 17:41:26 +0100

    Fix compilation warning: remove spurious spaces

--- src/asf-contexts-exceptions-iterator.adb
+++ src/asf-contexts-exceptions-iterator.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  asf-contexts-exceptions-iterator -- Exception handlers in faces context
---  Copyright (C) 2011, 2021 Stephane Carrez
+--  Copyright (C) 2011, 2021, 2022 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -59,5 +59,5 @@ package body ASF.Contexts.Exceptions.Iterator is
          end if;
       end loop;
    end Iterate;
-   
+
 end ASF.Contexts.Exceptions.Iterator;
